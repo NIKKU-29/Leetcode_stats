c150326e641179d9e503f67c6f1b9c6936331c8b
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-03 16:48:15 +0530

    Time: 7 ms (14.68%), Space: 10 MB (47.02%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-
         string number;
 
-        for(char elem : s)
-        {
-            number+= to_string(elem - 'a' + 1);
+        // Convert each character in the string to its corresponding number
+        for (auto elem : s) {
+            number += to_string(elem - 'a' + 1);
         }
 
-        
-        
-        while(k!=0)
-        {
-            int no=0;
+        // Convert the string to an integer if possible, else sum digits in string directly
+        int num = 0;
+        for (char digit : number) {
+            num += digit - '0';
+        }
 
-            for(char elem : number)
-            {
-                no+=(elem - '0'); 
+        // Perform the transformation `k` times
+        while (k > 1) {
+            int new_num = 0;
+            while (num > 0) {
+                new_num += num % 10;
+                num /= 10;
             }
-
-            number=to_string(no);
+            num = new_num;
             k--;
         }
 
-        return stoi(number);
+        return num;
     }
 };
