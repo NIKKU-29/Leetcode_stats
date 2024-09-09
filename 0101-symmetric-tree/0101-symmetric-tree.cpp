ec125d4493eb2b1c30175a62905c97e5e857dddb
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-09 23:58:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool solver(TreeNode* left,Treenode* right)
+    {
+        if(left == nullptr && right == nullptr) return true;
+        if(left == nullptr || right == nullptr) return false;
+
+        return (left->val == right->val) && solver(left->left,->right) && solver(right->left,left->right); 
+    }
+
+
+    bool isSymmetric(TreeNode* root) {
+
+            if(root==nullptr) return true;
+            return solver(root->left,root->right);
+    }
+};
