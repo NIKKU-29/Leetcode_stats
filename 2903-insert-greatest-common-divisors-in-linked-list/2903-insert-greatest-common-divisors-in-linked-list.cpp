309600fe64b586168f3d423baa76c642a433c95f
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-10 12:44:34 +0530

    Time: 39 ms (60.32%), Space: 35.7 MB (9.8%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
@@ -8,6 +8,14 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
