55829b30780f7540c2d90f82b6da765eada0a187
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-14 12:44:58 +0530

    Time: 96 ms (82.68%), Space: 84.9 MB (77.85%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        // Find the maximum element in the array
+        int target = *max_element(nums.begin(), nums.end());
         
-    
-        int target= * max_element(nums.begin(),nums.end());
-        int maxfreq=0;
-        int currfreq=0;
-    
-
-        for(int i=0;i<nums.size();i++)
-        {
-            
-            if(nums[i]==target)
-            {
-                currfreq++;
-                maxfreq=max(maxfreq,currfreq);
-            }
-
-            else{
-                currfreq=0;
-            }
-            
+        int maxfreq = 0; // To store the longest subarray length
+        int currfreq = 0; // To track the current subarray length
+        
+        // Iterate through the array once (O(n) time complexity)
+        for (int num : nums) {
+            // Increment currfreq if num equals target, else reset it
+            currfreq = (num == target) ? currfreq + 1 : 0;
+            // Update maxfreq with the maximum value of currfreq found so far
+            maxfreq = max(maxfreq, currfreq);
         }
 
-            return maxfreq;
+        return maxfreq;
     }
 };
