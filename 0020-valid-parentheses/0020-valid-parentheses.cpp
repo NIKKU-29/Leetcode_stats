4ce9e55b97af15e474d776a7021551678a7ac5f5
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-15 19:40:45 +0530

    Time: 3 ms (40.74%), Space: 7.9 MB (58.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char>st;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(!st.empty() && ( st.top()=='(' && s[i]==')' || st.top()=='{' && s[i]=='}' || st.top()=='[' && s[i]==']') )
+                {
+                        st.pop();
+                }
+
+            else{
+
+                st.push(s[i]);
+            }
+            
+            
+        }
+
+
+        if(st.empty()) return true;
+
+        else return false;
+        
+    }
+};
