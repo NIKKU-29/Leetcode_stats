6f9e7b8f5550758345ca0206526f5a448cbd96c1
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-20 20:23:23 +0530

    Time: 7 ms (64.09%), Space: 9.5 MB (92.97%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+
+        string compare=s;
+        int n=s.size();
+        if(n==0) return s;
+        reverse(compare.begin(),compare.end());
+        string prefix="";
+
+
+        for(int i=0;i<n;i++)
+        {
+            if(memcmp(s.c_str(),compare.c_str()+i,n-i)==0)
+            {
+                prefix=compare.substr(0,i);
+                break;
+            }
+        }
+
+
+        return prefix + s;        
+    }
+};
