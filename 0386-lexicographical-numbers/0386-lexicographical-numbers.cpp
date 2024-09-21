a40a3158e475522e286dabe3d509d774bf6ca6f8
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-21 14:20:07 +0530

    Time: 6 ms (78.34%), Space: 13.8 MB (55.23%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -15,33 +15,70 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<int> lexicalOrder(int n) {
+
+//         vector<int>Order;
+//         int currnum=1;
+
+//         for(int i=0;i<n;i++)
+//         {
+//             Order.push_back(currnum);
+
+//             if(currnum*10 <= n)
+//             {
+//                 currnum*=10;
+//             }
+
+//             else
+//             {
+//                 while(currnum%10 == 9 || currnum>=n)
+//                 {
+//                     currnum/=10;
+//                 }
+                
+//                 currnum+=1;
+//             }
+//         }
+
+//         return Order;
+//     }
+// };
+
 class Solution {
 public:
+
+    void solve(int curr,int n,vector<int>& ans)
+    {
+        if(curr>n) return;
+
+        ans.push_back(curr);
+
+
+        for(int i=0;i<=9;i++)
+        {
+            if(curr*10 + i > n)
+            {
+                return;
+            }
+
+            solve(curr*10 + i , n , ans);
+        }
+    }
+
+
     vector<int> lexicalOrder(int n) {
 
-        vector<int>Order;
-        int currnum=1;
+        vector<int>ans;
 
-        for(int i=0;i<n;i++)
+        for(int start=1;start<=9;start++)
         {
-            Order.push_back(currnum);
-
-            if(currnum*10 <= n)
-            {
-                currnum*=10;
-            }
-
-            else
-            {
-                while(currnum%10 == 9 || currnum>=n)
-                {
-                    currnum/=10;
-                }
-                
-                currnum+=1;
-            }
+            solve(start,n,ans);
         }
 
-        return Order;
+
+        return ans;
+        
     }
 };
