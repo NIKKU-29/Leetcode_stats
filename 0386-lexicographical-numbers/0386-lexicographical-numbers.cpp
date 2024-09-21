2a6552a3c8bd20cfeb697cc0096e45e806ce9a42
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-21 13:55:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,20 +1,47 @@
+// class Solution {
+// public:
+
+//     static bool comp(const int & a, const int & b) {
+//         return to_string(a) < to_string(b);
+//     }
+    
+//     vector<int> lexicalOrder(int n) {
+
+//         vector<int>vec(n);
+//         iota(vec.begin(),vec.end(),1);
+//         sort(vec.begin(),vec.end(),comp);
+//         return vec;
+        
+//     }
+// };
+
 class Solution {
 public:
-
-    static bool comp(const int & a, const int & b)
-    {
-         return to_string(a) < to_string(b);
-    }
-
     vector<int> lexicalOrder(int n) {
 
-        vector<int>vec(n);
-        iota(vec.begin(),vec.end(),1);
+        vector<int>Order;
+        int currnum=1;
 
-        sort(vec.begin(),vec.end(),comp);
+        for(int i=0;i<n;i++)
+        {
+            Order.push_back(currnum);
 
+            if(currnum*10 <= n)
+            {
+                currnum*=10;
+            }
 
-        return vec;
-        
+            else
+            {
+                if(currnum%10 == 9 || currnum>=n)
+                {
+                    currnum/=10;
+                }
+                
+                currnum+=1;
+            }
+        }
+
+        return Order;
     }
 };
