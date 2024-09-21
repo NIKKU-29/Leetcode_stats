a649ca60a00bce78069c2f69b4830b91aa2e9441
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-21 18:37:22 +0530

    Time: 1895 ms (8.41%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select Product.product_name , Sales.year , Sales.price
-from Sales 
-INNER JOIN Product ON Product.product_id=Sales.product_id;
+select p.product_name , s.year , s.price
+from Sales s , Product p
+where p.product_id=s.product_id;
 
