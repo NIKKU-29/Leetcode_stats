73a0da70eb9a1bee28d85a88e8c26815f5ef2ac3
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-09-27 03:35:21 +0530

    Time: 71 ms (81.91%), Space: 42.8 MB (36.23%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,31 +1,55 @@
-class MyCalendar {
+// class MyCalendar {
 
-private: 
-            map<int,int>event;
-public:
-    MyCalendar() {
+// private: 
+//             map<int,int>event;
+// public:
+//     MyCalendar() {
         
-    }
+//     }
     
-    bool book(int start, int end) {
+//     bool book(int start, int end) {
 
-        event[start]++;
-        event[end]--;
+//         event[start]++;
+//         event[end]--;
 
-        int current=0;
+//         int current=0;
 
-        for(auto & elem : event)
-        {
-            current += elem.second;
+//         for(auto & elem : event)
+//         {
+//             current += elem.second;
 
-            if(current > 1)
-            {
-                event[start]--;
-                event[end]++;
-                return false;
-            }
-        }
+//             if(current > 1)
+//             {
+//                 event[start]--;
+//                 event[end]++;
+//                 return false;
+//             }
+//         }
         
+//         return true;
+//     }
+// };
+
+// /**
+//  * Your MyCalendar object will be instantiated and called as such:
+//  * MyCalendar* obj = new MyCalendar();
+//  * bool param_1 = obj->book(start,end);
+//  */
+
+class MyCalendar {
+    map<int, int> intervals; // Stores intervals with start as key and end as value
+public:
+    MyCalendar() {}
+
+    bool book(int start, int end) {
+        auto next = intervals.lower_bound(start); // Find next interval
+        if (next != intervals.end() && next->first < end) {
+            return false; // Overlaps with next interval
+        }
+        if (next != intervals.begin() && prev(next)->second > start) {
+            return false; // Overlaps with previous interval
+        }
+        intervals[start] = end; // No overlap, add interval
         return true;
     }
 };
