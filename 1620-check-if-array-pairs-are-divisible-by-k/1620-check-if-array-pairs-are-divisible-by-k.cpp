4bc057a2467c0a053fd71550f519fd6b4153106d
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-01 17:54:17 +0530

    Time: 130 ms (45.37%), Space: 73.5 MB (63.09%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        unordered_map<int, int> mp;
-        int paircount = 0;
 
-        // Count occurrences of remainders when divided by k
-        for (int elem : arr) {
-            int mod = (elem % k + k) % k;  // To handle negative values correctly
+        unordered_map<int,int>mp;
+
+        for(auto elem : arr)
+        {
+            int mod= ( elem % k + k) % k;
             mp[mod]++;
         }
 
-        // Now check pairs based on remainders
-        for (int i = 0; i < arr.size(); i++) {
-            int mod = (arr[i] % k + k) % k;
 
-            if (mod == 0) {
-                // Special case for elements divisible by k (i.e., mod == 0)
-                if (mp[mod] % 2 != 0) {
-                    return false; // Cannot form pairs if odd count
-                }
-            } else {
-                int target = k - mod;  // We need another element whose mod is `k - mod`
-                if (mp[mod] != mp[target]) {
-                    return false;  // If counts don't match, pairs cannot be formed
-                }
+
+        for(int i=1;i<arr.size();i++)
+        {
+            int mod =  ( arr[i] % k + k) % k ;
+            int target = k - mod;
+        
+        if(mod==0)
+        {
+            if(mp[mod]%2!=0) return false;
+        }
+            
+        else if(mp[target]!=mp[mod])
+            {
+                return false;
             }
         }
 
-        return true;
+
+    return true;
+        
     }
 };
