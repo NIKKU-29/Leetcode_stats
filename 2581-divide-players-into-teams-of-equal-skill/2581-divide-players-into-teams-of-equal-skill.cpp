3c2343e19d49e1f57fa2a17372173a76004dfb03
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-04 10:22:40 +0530

    Time: 63 ms (62%), Space: 56.5 MB (56.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    
     typedef long long ll;
 
     ll dividePlayers(vector<int>& skill) {
+        int n = skill.size();
+        sort(skill.begin(), skill.end());  // Sort the skills in ascending order
 
-        int n=skill.size();
-        unordered_map<int,int>mp;
- 
-        int paircount=n/2;
-        int Total=0;
+        int paircount = n / 2;
+        ll Total = accumulate(skill.begin(), skill.end(), 0LL);  // Sum of all skills
 
-        for(auto & elem : skill)
-        {
-            mp[elem]++;
-            Total+=elem;
+        // Check if total can be evenly divided
+        if (Total % paircount != 0) {
+            return -1;  // Early exit if pairing is impossible
         }
 
-        int Target=Total/paircount;
-        ll ans=0;
+        int Target = Total / paircount;
+        ll ans = 0;
 
-        for(int i=0;i<n;i++)
-        {
-            if(mp[skill[i]] <= 0) continue;
+        // Use two pointers to pair the players
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = skill[left] + skill[right];
 
-            int Aim=Target-skill[i];
-            if(mp.find(Aim)!=mp.end() && mp[Aim]>0)
-            {
-                ans+=Aim * skill[i];
-                mp[skill[i]]--;
-                mp[Aim]--;
-
+            if (sum != Target) {
+                return -1;  // If pairing is impossible
             }
-            else{
-                return -1;
-            }
+
+            // Accumulate the product of pairs
+            ans += (ll)skill[left] * skill[right];
+
+            // Move pointers inward
+            left++;
+            right--;
         }
 
         return ans;
-
     }
 };
