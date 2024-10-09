4c41b949e3d9ac1db4fefbb13d75cefb2dcdf06f
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-09 09:48:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,31 +1,58 @@
+// class Solution {
+// public:
+//     int minAddToMakeValid(string s) {
+
+//         stack<char>st;
+
+//         for(auto elem : s)
+//         {
+//             if(st.empty())
+//             {
+//                 st.push(elem);
+//             }
+
+//             else{
+
+//                     if(st.top()=='(' && elem == ')')
+//                     {
+//                         st.pop();
+//                     }
+
+//                     else{
+
+//                         st.push(elem);
+//                     }
+
+//             }
+//         }
+
+//         return st.size();
+//     }
+// };
+
+
 class Solution {
 public:
     int minAddToMakeValid(string s) {
 
-        stack<char>st;
+        int o_count=0;
+        int adds=0;
 
         for(auto elem : s)
         {
-            if(st.empty())
+            if(elem == '(')
             {
-                st.push(elem);
+                o_count++;
             }
 
             else{
 
-                    if(st.top()=='(' && elem == ')')
-                    {
-                        st.pop();
-                    }
-
-                    else{
-
-                        st.push(elem);
-                    }
-
+                if(o_count > 0) o_count--;
+                else adds++;
             }
         }
 
-        return st.size();
+
+        return o_count+adds;
     }
 };
