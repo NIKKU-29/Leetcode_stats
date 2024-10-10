3ea6e6068485f527c7926200847d3a1b4be3f62f
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-10 22:56:12 +0530

    Time: 3 ms (53.12%), Space: 9.3 MB (56.93%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,38 +1,71 @@
+// class Solution {
+// public:
+
+//     unordered_set<int>holder;
+//     int n;
+
+//     void solver(vector<int>& nums,vector<int>&temp,vector<vector<int>>&ans)
+//     {
+//         if(temp.size()==n)
+//         {
+//             ans.push_back(temp);
+//         }
+
+//         for(int i=0;i<n;i++)
+//         {
+//             if(!holder.count(nums[i]))
+//             {
+//             temp.push_back(nums[i]);
+//             holder.insert(nums[i]);
+//             solver(nums,temp,ans);
+//             holder.erase(nums[i]);
+//             temp.pop_back();
+
+//             }
+
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+
+//         n=nums.size();
+//         vector<vector<int>>ans;
+//         vector<int>temp;
+
+//         solver(nums,temp,ans);
+
+//         return ans;
+        
+//     }
+// };
+
 class Solution {
 public:
 
-    unordered_set<int>holder;
     int n;
+    vector<vector<int>>ans;
 
-    void solver(vector<int>& nums,vector<int>&temp,vector<vector<int>>&ans)
+
+    void solver(int idx,vector<int>& nums)
     {
-        if(temp.size()==n)
+        if(idx==n)
         {
-            ans.push_back(temp);
+            ans.push_back(nums);
+            return;
         }
 
-        for(int i=0;i<n;i++)
+        for(int i=idx;i<n;i++)
         {
-            if(!holder.count(nums[i]))
-            {
-            temp.push_back(nums[i]);
-            holder.insert(nums[i]);
-            solver(nums,temp,ans);
-            holder.erase(nums[i]);
-            temp.pop_back();
-
-            }
-
+            swap(nums[i],nums[idx]);
+            solver(idx+1,nums);
+            swap(nums[idx],nums[i]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
 
         n=nums.size();
-        vector<vector<int>>ans;
-        vector<int>temp;
-
-        solver(nums,temp,ans);
+        solver(0,nums);
 
         return ans;
         
