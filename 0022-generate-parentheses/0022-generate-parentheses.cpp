c03675be1187f3919267be6e3e30a6976012ba5e
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-10 21:21:29 +0530

    Time: 0 ms (100%), Space: 15.9 MB (24.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,61 +1,106 @@
+// class Solution {
+// public:
+
+
+//     bool isvalid(string& current)
+//     {
+//        int counter=0;
+
+//         for(auto elem : current)
+//         {
+//             if(elem=='(')
+//             {
+//                 counter++;
+//             }
+
+//             else if(counter>0 && elem ==')')
+//             {
+//                 counter--;
+//             }
+
+//             else{
+//                 return false;
+//             }
+
+//         }
+
+//         if(counter!=0) return false;
+
+//         return true;
+//     }
+
+//     void solver(string curr , int siz , vector<string>&ans)
+//     {
+//         if(curr.size()==siz)
+//         {
+//             if(isvalid(curr))
+//             {
+//                 ans.push_back(curr);
+//             }
+
+//             return;
+//         }
+
+//         curr.push_back('(');
+//         solver(curr,siz,ans);
+//         curr.pop_back();
+//         curr.push_back(')');
+//         solver(curr,siz,ans);
+//         curr.pop_back();
+
+        
+//     }
+
+//     vector<string> generateParenthesis(int n) {
+
+//         string curr="";
+//         vector<string>ans;
+//         solver(curr,n*2,ans);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
 
+ void solver(string curr , int siz ,int open,int  close, vector<string>&ans)
 
-    bool isvalid(string& current)
-    {
-       int counter=0;
-
-        for(auto elem : current)
-        {
-            if(elem=='(')
-            {
-                counter++;
-            }
-
-            else if(counter>0 && elem ==')')
-            {
-                counter--;
-            }
-
-            else{
-                return false;
-            }
-
-        }
-
-        if(counter!=0) return false;
-
-        return true;
-    }
-
-    void solver(string curr , int siz , vector<string>&ans)
     {
         if(curr.size()==siz)
         {
-            if(isvalid(curr))
-            {
-                ans.push_back(curr);
-            }
-
+            ans.push_back(curr);
             return;
         }
 
+
+        if(open < siz/2)
+        {
         curr.push_back('(');
-        solver(curr,siz,ans);
+        solver(curr,siz,open+1,close,ans);
         curr.pop_back();
+
+        }
+
+        if(close < open)
+        {
         curr.push_back(')');
-        solver(curr,siz,ans);
+        solver(curr,siz,open,close+1,ans);
         curr.pop_back();
+        }
+
 
         
     }
 
+
     vector<string> generateParenthesis(int n) {
 
         string curr="";
+        int open=0;
+        int close=0;
         vector<string>ans;
-        solver(curr,n*2,ans);
+        solver(curr,n*2,0,0,ans);
         return ans;
     }
 };
