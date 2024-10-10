28325d436075ef20e848d464aa473bb0ddc1a575
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-10 23:01:49 +0530

    Time: 7 ms (10.05%), Space: 10.3 MB (33.33%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,73 +1,74 @@
-// class Solution {
-// public:
-
-//     unordered_set<int>holder;
-//     int n;
-
-//     void solver(vector<int>& nums,vector<int>&temp,vector<vector<int>>&ans)
-//     {
-//         if(temp.size()==n)
-//         {
-//             ans.push_back(temp);
-//         }
-
-//         for(int i=0;i<n;i++)
-//         {
-//             if(!holder.count(nums[i]))
-//             {
-//             temp.push_back(nums[i]);
-//             holder.insert(nums[i]);
-//             solver(nums,temp,ans);
-//             holder.erase(nums[i]);
-//             temp.pop_back();
-
-//             }
-
-//         }
-//     }
-
-//     vector<vector<int>> permute(vector<int>& nums) {
-
-//         n=nums.size();
-//         vector<vector<int>>ans;
-//         vector<int>temp;
-
-//         solver(nums,temp,ans);
-
-//         return ans;
-        
-//     }
-// };
-
 class Solution {
 public:
 
+    unordered_set<int>holder;
     int n;
-    vector<vector<int>>ans;
 
-
-    void solver(int idx,vector<int>& nums)
+    void solver(vector<int>& nums,vector<int>&temp,vector<vector<int>>&ans)
     {
-        if(idx==n)
+        if(temp.size()==n)
         {
-            ans.push_back(nums);
+            ans.push_back(temp);
             return;
         }
 
-        for(int i=idx;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            swap(nums[i],nums[idx]);
-            solver(idx+1,nums);
-            swap(nums[idx],nums[i]);
+            if(!holder.count(nums[i]))
+            {
+            temp.push_back(nums[i]);
+            holder.insert(nums[i]);
+            solver(nums,temp,ans);
+            holder.erase(nums[i]);
+            temp.pop_back();
+
+            }
+
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
 
         n=nums.size();
-        solver(0,nums);
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+        solver(nums,temp,ans);
 
         return ans;
         
     }
 };
+
+// class Solution {
+// public:
+
+//     int n;
+//     vector<vector<int>>ans;
+
+
+//     void solver(int idx,vector<int>& nums)
+//     {
+//         if(idx==n)
+//         {
+//             ans.push_back(nums);
+//             return;
+//         }
+
+//         for(int i=idx;i<n;i++)
+//         {
+//             swap(nums[i],nums[idx]);
+//             solver(idx+1,nums);
+//             swap(nums[idx],nums[i]);
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+
+//         n=nums.size();
+//         solver(0,nums);
+
+//         return ans;
+        
+//     }
+// };
