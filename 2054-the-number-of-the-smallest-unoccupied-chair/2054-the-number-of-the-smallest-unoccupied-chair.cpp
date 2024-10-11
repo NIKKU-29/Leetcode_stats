0ab30038edf24dabab1272c245e843ed8fc008eb
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-11 13:36:31 +0530

    Time: 118 ms (98.97%), Space: 58.9 MB (81.79%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,36 +1,88 @@
+// class Solution {
+// public:
+//     int smallestChair(vector<vector<int>>& times, int targetFriend) {
+
+//         int n=times.size();
+//         vector<int>chairs(n,-1);
+
+//         int target=times[targetFriend][0];
+//         sort(times.begin(),times.end());
+
+//         for(auto elem : times)
+//         {
+//             int arrival=elem[0];
+//             int dpart=elem[1];
+
+//             for(int i=0 ; i<n ;i++)
+//             {
+//                 if(chairs[i] <= arrival)
+//                 {
+//                     chairs[i]=dpart;
+
+//                     if(arrival==target)
+//                     {
+//                         return i;
+//                     }
+
+//                     break;
+//                 }
+
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
+    typedef pair<int, int> P;
+    typedef int i;
+
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
 
-        int n=times.size();
-        vector<int>chairs(n,-1);
+        int n = times.size();
 
-        int target=times[targetFriend][0];
-        sort(times.begin(),times.end());
+        priority_queue<P, vector<P>, greater<P>> occupied;
+        priority_queue<i, vector<i>, greater<i>> chairs;
 
+        int target = times[targetFriend][0];
 
-        for(auto elem : times)
-        {
-            int arrival=elem[0];
-            int dpart=elem[1];
+        sort(begin(times), end(times));
 
-            for(int i=0 ; i<n ;i++)
-            {
-                if(chairs[i] <= arrival)
-                {
-                    chairs[i]=dpart;
+        int chair_num = 0;
 
-                    if(arrival==target)
-                    {
-                        return i;
-                    }
+        for (auto elem : times) {
+            int arrival = elem[0];
+            int depart = elem[1];
 
-                    break;
+            while (!occupied.empty() && occupied.top().first <= arrival) {
+                chairs.push(occupied.top().second);
+                occupied.pop();
+            }
+
+            if (chairs.empty()) {
+                occupied.push({depart, chair_num});
+
+                if (arrival == target) {
+                    return chair_num;
                 }
 
+                chair_num++;
+
+            }
+
+            else {
+
+                int givenchair = chairs.top();
+                chairs.pop();
+
+                if (arrival == target) {
+                    return givenchair;
+                }
+                occupied.push({depart, givenchair});
             }
         }
-
 
         return -1;
     }
