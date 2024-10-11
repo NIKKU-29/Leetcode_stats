c1630e4938343a8027be6ce2c268b94172a26eee
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-11 18:09:20 +0530

    Time: 7 ms (32.22%), Space: 23.8 MB (5.04%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int t[21][21];
+
+    bool solve(int i, int j, string s, string p) {
+        if (j == p.length()) {
+            return i == s.length();
+        }
+
+        bool first_match = false;
+
+        if(t[i][j]!=-1) return t[i][j];
+
+        if (i < s.length() && (p[j] == s[i] || p[j] == '.')) {
+            first_match = true;
+        }
+
+        if ( j+1 < p.length() && p[j + 1] == '*') {
+            bool take = first_match && solve(i + 1, j, s, p);
+            bool notake = solve(i, j + 2, s, p);
+
+            return  t[i][j]= take || notake;
+        }
+
+        return  t[i][j]= first_match && solve(i + 1, j + 1, s, p);
+    }
+
+    bool isMatch(string s, string p) { 
+        memset(t , -1, sizeof(t));
+        return solve(0, 0, s, p); }
+};
