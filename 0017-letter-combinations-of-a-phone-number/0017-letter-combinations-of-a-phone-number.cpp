2d465ab384abee3adaf762233d2383560a3a5e98
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-14 17:41:10 +0530

    Time: 4 ms (16.07%), Space: 9 MB (12.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
-
-    void backtrack(string& digits,unordered_map<char,string>&mp,int index,string current,vector<string>&ans)
-    {
-        if(index==digits.size())
+        void solver(int i , string temp,string digits,unordered_map<int,string>&mp,vector<string>&ans)
         {
-            ans.push_back(current);
-            return;
-        }
 
-        char digit = digits[index];
-        string letters = mp[digit];
+            if(i==digits.size())
+            {
+                ans.push_back(temp);
+                return;
+            }
+            
 
+            char elem = digits[i];
+            string act=mp[elem];
 
-        for(char elem : letters)
-        {
-            backtrack(digits,mp,index+1,current + elem ,ans);
-        }
-    }
-
-    vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> mp;
-        char elem = 'a';
-        vector<string> ans;
-
-        if (digits.empty()) return {};
-
-        for (char i = '2'; i <= '9'; i++) { // Start from '2' to '9'
-            string s = ""; // Reset string for each digit
-            int counter = 0;
-
-            if (i == '7' || i == '9') { // 7 and 9 have 4 letters
-                while (counter < 4) {
-                    s += elem;
-                    elem++;
-                    counter++;
-                }
-            } else { // 2, 3, 4, 5, 6, 8 have 3 letters
-                while (counter < 3) {
-                    s += elem;
-                    elem++;
-                    counter++;
-                }
+            for(auto ele : act)
+            {
+                solver(i+1,temp + ele,digits,mp,ans);
             }
 
-            mp[i] = s; // Store the corresponding letters for each digit
+            
         }
 
-        backtrack(digits, mp, 0, "", ans); // Start backtracking
-        return ans; // Return the result
+
+    vector<string> letterCombinations(string digits) {
+
+        if(digits.empty()) return {};
+
+        unordered_map<int,string>mp;
+
+            mp['2']="abc";
+            mp['3']="def";
+            mp['4']="ghi";
+            mp['5']="jkl";
+            mp['6']="mno";
+            mp['7']="pqrs";
+            mp['8']="tuv";
+            mp['9']="wxyz";
+    
+        vector<string>ans;
+      
+        solver(0,"",digits,mp,ans);
+
+        return ans;
+        
     }
 };
