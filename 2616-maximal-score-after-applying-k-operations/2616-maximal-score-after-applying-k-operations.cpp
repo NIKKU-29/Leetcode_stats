ffd592218a3265b06f8833ac166894917425a36c
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-14 15:22:40 +0530

    Time: 156 ms (95.26%), Space: 76.1 MB (79.55%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,34 +1,51 @@
+// class Solution {
+// public:
+
+       
+
+//     long long maxKelements(vector<int>& nums, int k) {
+
+//         priority_queue<int>pq(nums.begin(),nums.end());
+//         long long ans=0;
+
+//         for(int i=0;i<k;i++)
+//         {
+//             int elem=pq.top();
+//             ans+=elem;
+
+//             if(elem==1)
+//             {
+//                 ans+=(k-elem-1);
+//                 break;
+//             }
+
+//             pq.pop();
+//             pq.push((elem+2)/3);
+           
+//         }
+
+
+//         return ans;
+        
+//     }
+// };
+
+
 class Solution {
 public:
-
-        typedef long long l;
-
-    l maxKelements(vector<int>& nums, int k) {
-
-        priority_queue<l>pq;
-        l ans=0;
-
-
-        for(l elem : nums)
-        {
-            pq.push(elem);
-        }
-
-        while(k)
-        {
-            l elem=pq.top();
-            cout<<elem<<"-->";
-            ans+=elem;
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        long long score=0;
+        for(int i=0; i<k; i++){
+            int x=pq.top();
+            score+=x;
+            if (x==1){
+                score+=(k-1-i);
+                break;
+            }
             pq.pop();
-            l val=ceil((double)elem/3);
-            cout<<val;
-            cout<<endl;
-            pq.push(val);
-            k--;
+            pq.push((x+2)/3);
         }
-
-
-        return ans;
-        
+        return score;
     }
 };
