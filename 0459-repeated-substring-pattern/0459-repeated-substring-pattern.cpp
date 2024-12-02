71c595f1c740efb3ea99af0ecd3108b7ac39dab9
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-03 02:48:32 +0530

    Time: 12 ms (52.48%), Space: 15 MB (55.88%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
 
+//APPROACH 1 - DOUBLE THE ORIGNAL STRING REMOVE 1st AND Nth Character 
+//             FIND STRING IN IT
+
      //a .bc|abcabcabcab|cabcabcab. c
      //     |abcabcabcab|
+
+ bool repeatedSubstringPattern(string s) {
+
+    string doubled=s+s;
+    string removedEnds=doubled.substr(1,doubled.size()-2);
+    if(removedEnds.find(s)!=string::npos) return true;
+    return false;
      
 
-    bool repeatedSubstringPattern(string s) {
 
-        int n=s.size();
-        for(int i=1 ; i<= n/2 ; i++)
-        {
-            if(n % i == 0) //uneven length
-            {
-                string curr=s.substr(0,i);
-                string concatinated="";
-                for(int j=0 ; j< n/i ; j++)
-                {
-                    concatinated+=curr;
-                }
+//APPROACH 2 - TAKE THE STRING~SUBSTRINGS ONE BY ONE
 
-                if(concatinated==s) return true;
-            }
-        }
+
+    // bool repeatedSubstringPattern(string s) {
+
+    //     int n=s.size();
+    //     for(int i=1 ; i<= n/2 ; i++)
+    //     {
+    //         if(n % i == 0) //uneven length
+    //         {
+    //             string curr=s.substr(0,i);
+    //             string concatinated="";
+    //             for(int j=0 ; j< n/i ; j++)
+    //             {
+    //                 concatinated+=curr;
+    //             }
+
+    //             if(concatinated==s) return true;
+    //         }
+    //     }
         
-        return false;
+    //     return false;
     }
 };
