79ec8d93642eb7ac19cc17800fa4fed5d6993880
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-03 19:06:05 +0530

    Time: 19 ms (63.55%), Space: 85.4 MB (19.63%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+
+        string ans="";
+
+        int n=s.size();
+        int m=spaces.size();
+        int j=0;
+        for(int i=0 ; i< n ;i++)
+        {
+            if(j<m && i == spaces[j])
+            {
+                ans+=" ";
+                j++;
+                i--;
+            }
+
+            else{
+                ans+=s[i];
+            }
+        }
+
+        return ans;
+        
+    }
+};
