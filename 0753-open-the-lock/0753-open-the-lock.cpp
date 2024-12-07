aec2e6945879d15ef2215ce5a8fd6dd771b51d09
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-08 00:53:58 +0530

    Time: 159 ms (52.18%), Space: 38.4 MB (74.82%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.cpp
+++ 0753-open-the-lock/0753-open-the-lock.cpp
@@ -1,41 +1,52 @@
-class Solution 
-{
+class Solution {
 public:
-    int openLock(vector<string>& deadends, string target) 
-    {
-        int level = 0;
-        unordered_set<string> dead, vis;
+    int openLock(vector<string>& deadends, string target) {
+            int moves=0;
+            unordered_set<string>st,vis;
 
-        for(auto &deadend : deadends) dead.insert(deadend);
-        if(dead.count("0000")) return -1;
-        
+            for(auto elem : deadends) st.insert(elem);
+            queue<string>q;
+            q.push("0000");
+            vis.insert("0000");
 
-        queue<string> q;
-        q.push("0000");
-        vis.insert("0000");
+            if(st.count("0000")) return -1;
 
-        while(!q.empty())
-        {
-            int sz = q.size();
-            while(sz--)
+            while(!q.empty())
             {
-                string cur_state = q.front();q.pop();
-                if(cur_state == target) return level;
-                for(int i = 0; i < 4; i++)
-                {
-                    string temp = cur_state;
-                    temp[i] == '9' ? temp[i] = '0' : temp[i]++;
-                    if(!vis.count(temp) && !dead.count(temp)) 
-                        q.push(temp), vis.insert(temp);
+                int sz=q.size();
 
-                    temp = cur_state;
-                    temp[i] == '0' ? temp[i] = '9' : temp[i]--;
-                    if(!vis.count(temp) && !dead.count(temp)) 
-                        q.push(temp), vis.insert(temp);
+                while(sz--)
+                {
+                    string curr=q.front();
+                    q.pop();
+
+                    if(curr==target) return moves;
+
+                    for(int i=0 ; i < 4; i++)
+                    {
+                        string temp=curr;
+                        temp[i]=='9' ? temp[i]= '0' : temp[i]++;
+                        if(!st.count(temp) && !vis.count(temp))
+                        {
+                            q.push(temp);
+                            vis.insert(temp);
+                        }
+                        
+                        temp=curr;
+                        temp[i]=='0' ? temp[i] = '9' : temp[i]--;
+                        if(!st.count(temp) && !vis.count(temp))
+                        {
+                            q.push(temp);
+                            vis.insert(temp);
+                        }   
+                    }
+
                 }
+                    ++moves;
+                
             }
-            ++level;
-        }
-        return -1;
+
+            return -1;
+
     }
 };
