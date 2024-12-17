e88a8f40605da6e9290890c9cd668d1d4de93a53
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-17 23:24:15 +0530

    Time: 59 ms (35.7%), Space: 28.3 MB (21.08%) - LeetHub

---
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+
+    void helper(priority_queue<pair<char,int>>&pq,string &ans,pair<char, int> elem,char &prevElem,int repeatLimit)
+    {
+        if(elem.second <= repeatLimit)
+            {
+                ans.append(elem.second, elem.first);
+                prevElem=elem.first;
+            }
+
+            else
+            {
+                    ans.append(repeatLimit,elem.first);
+                    if(elem.second-repeatLimit!=0)
+                    {
+                        pq.push({elem.first,elem.second-repeatLimit});
+                    }    
+                    prevElem=elem.first; 
+                 
+
+            }
+    }
+
+    string repeatLimitedString(string s, int repeatLimit) {
+
+        priority_queue<pair<char,int>>pq;
+        unordered_map<char,int>mp;
+
+
+        for(auto elem : s)
+        {
+            mp[elem]++;
+        }
+
+        for(auto elem : mp)
+        {
+            pq.push({elem.first,elem.second});
+        }
+
+        string ans="";
+        char prevElem='!';
+
+        while(!pq.empty())
+        {
+            auto elem = pq.top(); 
+            pq.pop();
+
+            if(elem.first!=prevElem) 
+            {
+                helper(pq,ans,elem,prevElem,repeatLimit);
+            }
+
+            //if elemnt on top is same as previous then add the next to the top to the string
+            else {
+                    if(pq.empty())
+                    {
+                        return ans;   
+                    }
+
+            auto Nelem = pq.top();
+            pq.pop();
+            
+                ans.append(1,Nelem.first);
+                prevElem=Nelem.first;
+
+                if (Nelem.second - 1 > 0) {
+                    pq.push({Nelem.first, Nelem.second - 1});
+                }
+
+                pq.push(elem);
+                    
+            }
+        }
+
+        return ans;
+        
+    }
+};
