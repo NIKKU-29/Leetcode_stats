b1b3104da01b91dfd983f22405f0cc9a31103760
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-18 20:53:28 +0530

    Time: 0 ms (100%), Space: 14 MB (28.27%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,25 +3,18 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
 
         int n = prices.size();
-        vector<int>ans;
-        
         for(int i=0 ; i < n-1 ; i++)
         {
-            bool done=false;
             for(int j=i+1; j < n ; j++)
             {
                 if(prices[j]<=prices[i])
                 {
-                    ans.push_back(prices[i]-prices[j]);
-                    done=true;
+                    prices[i]-=prices[j];
                     break;
                 }
             }
-
-            if(done==false) ans.push_back(prices[i]);
         }
-        ans.push_back(prices[n-1]);
-        return ans;
-        
+
+        return prices;
     }
 };
