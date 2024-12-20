0346b76cc8a4550c0b81055cb039837c64b84810
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-21 04:39:54 +0530

    Time: 7 ms (26.14%), Space: 77.1 MB (75.03%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,3 +1,61 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+
+//         int level=0;
+//         TreeNode* temp=root;
+//         queue<TreeNode*>q;
+//         q.push(temp);
+
+//         while(!q.empty())
+//         {
+//             int size=q.size();
+//             vector<TreeNode*>CurrNodes;
+
+
+//             while(size--)
+//             {
+//                 TreeNode* curr=q.front();
+//                 q.pop();
+
+//                 if(level % 2 == 1) //odd level
+//                 {
+//                     CurrNodes.push_back(curr);
+//                 }
+
+//                 if(curr->left) q.push(curr->left);
+//                 if(curr->right) q.push(curr->right);
+//             }
+        
+
+//         if(level%2==1)
+//         {
+//             int n=CurrNodes.size();
+
+//             for(int i=0 ; i < n/2 ; i++)
+//             {
+//                 swap(CurrNodes[i]->val,CurrNodes[n-i-1]->val);
+//             }   
+//         }
+
+//         ++level;
+//     }
+
+//         return root;
+
+//     }
+// };
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -11,48 +69,29 @@
  */
 class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
 
-        int level=0;
-        TreeNode* temp=root;
-        queue<TreeNode*>q;
-        q.push(temp);
-
-        while(!q.empty())
+    void DFS(TreeNode* CurrL , TreeNode* CurrR ,int level)
+    {
+        if(CurrL == nullptr || CurrR == nullptr )
         {
-            int size=q.size();
-            vector<TreeNode*>CurrNodes;
-
-
-            while(size--)
-            {
-                TreeNode* curr=q.front();
-                q.pop();
-
-                if(level % 2 == 1) //odd level
-                {
-                    CurrNodes.push_back(curr);
-                }
-
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
-            }
-        
-
-        if(level%2==1)
-        {
-            int n=CurrNodes.size();
-
-            for(int i=0 ; i < n/2 ; i++)
-            {
-                swap(CurrNodes[i]->val,CurrNodes[n-i-1]->val);
-            }   
+            return;
         }
 
-        ++level;
+        if(level%2==0) //even level next level is odd hence swap childs
+        {
+            swap(CurrL->val,CurrR->val);
+        }
+
+        DFS(CurrL->left,CurrR->right,level+1);
+        DFS(CurrL->right,CurrR->left,level+1);
     }
 
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        DFS(root->left,root->right,0);
         return root;
-
+        
     }
+
+
 };
