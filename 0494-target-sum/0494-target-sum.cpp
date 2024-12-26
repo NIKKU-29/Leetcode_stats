a805d722c4881e10db7d0c039148f5e1f5b3d699
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-26 23:57:44 +0530

    Time: 19 ms (54.77%), Space: 42.7 MB (20.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,40 @@
-
 class Solution {
 public:
-    int solver(int index, int curr, int target, vector<int>& nums, vector<vector<int>>& dp) { 
-        int n = nums.size();
-        if (index == n) {
-            if (curr == target) return 1;
+
+    
+
+    int solver(int idx,int currsum , vector<vector<int>>&dp , int target , vector<int>& nums)
+    {
+        int n=nums.size();
+        if(idx==n)
+        {
+            if(currsum==target)
+            {
+                return 1;
+            }
+
             return 0;
         }
-        if (dp[index][curr + 1000] != -1) {
-            return dp[index][curr + 1000];
+
+        if(dp[idx][currsum + 1000]!=-1)
+        {
+            return dp[idx][currsum+1000];
         }
 
-        int positive = solver(index + 1, curr + nums[index], target, nums, dp);
-        int negative = solver(index + 1, curr - nums[index], target, nums, dp);
 
-        return dp[index][curr + 1000] = positive + negative;
+        int plus=solver(idx+1,currsum+nums[idx],dp,target,nums);
+        int neg=solver(idx+1,currsum-nums[idx],dp,target,nums);
+
+        return dp[idx][currsum+1000]=plus + neg;
+        
     }
 
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2001, -1)); 
-        return solver(0, 0, target, nums, dp);
+
+        int n=nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2001, -1));
+        int count=solver(0,0,dp,target,nums);
+        return count;
     }
 };
