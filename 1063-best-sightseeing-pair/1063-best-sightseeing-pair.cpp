2f5bdcad9298281f047673c717a27360315e617d
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-12-28 05:26:36 +0530

    Time: 0 ms (100%), Space: 45.6 MB (42.04%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int n = values.size();
-        int maxAdded = values[0];  // Keep track of the maximum (values[i] + i) seen so far
-        int maxScore = INT_MIN;   // Initialize the maximum score
-        
-        for (int j = 1; j < n; ++j) {
-            // Update the maximum score using the current `j`
-            maxScore = max(maxScore, maxAdded + values[j] - j);
-            
-            // Update maxAdded for the next iteration
-            maxAdded = max(maxAdded, values[j] + j);
+
+        int n=values.size();
+        int add=values[0];
+        int ans=INT_MIN;
+
+
+        for(int i=1 ; i< n ;i++)
+        {
+            ans=max(ans, add + values[i]-i);
+            add=max(add,values[i]+i);
         }
+
+        return ans;
         
-        return maxScore;
     }
 };
