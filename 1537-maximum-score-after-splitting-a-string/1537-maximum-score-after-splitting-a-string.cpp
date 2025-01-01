4d61250d37d40dcdc7ff641ee3ffaa1b5e659494
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-01 10:37:43 +0530

    Time: 0 ms (100%), Space: 8.8 MB (38.26%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxScore(string s) {
+
+        int count0=0;
+        int count1=0;
+        int n=s.size();
+
+        for(auto elem : s)
+        {
+            if(elem=='1') count1++;
+        }
+
+        if(s[0]=='0') count0++;
+        if(s[0]=='1') count1--;
+
+        int maxi=INT_MIN;
+
+        for(int i=1 ; i< n ;i++)
+        {
+            maxi=max(maxi,(count0+count1));
+            if(s[i]=='1') count1--;
+            if(s[i]=='0') count0++;   
+        }
+
+        return maxi;
+        
+    }
+};
