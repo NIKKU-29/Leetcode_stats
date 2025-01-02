e15bfbef056a8fbb6cd71beb2ca38a3f00c747db
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-02 15:23:37 +0530

    Time: 24 ms (21.71%), Space: 74.9 MB (13.88%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
 
-           
+        set<char>st={'a','e','i','o','u'};    
         int n=words.size();
-        vector<int>prefix{0};
+        vector<int>prefix;
         vector<int>answer;
 
         int count=0;
         for(auto& elem : words){
             
             int l=elem.size();
-            if((elem[0] == 'a' || elem[0] == 'e' || elem[0] == 'i' || elem[0] == 'o' || elem[0] == 'u') && (elem[l-1] == 'a' || elem[l-1] == 'e' || elem[l-1] == 'i' || elem[l-1] == 'o' || elem[l-1] == 'u'))
+            if(st.count(elem[0]) && st.count(elem[l-1]))
             {
                 count++;
             }
@@ -21,7 +21,20 @@ public:
 
         for(auto elem : queries)
         {
-            answer.push_back(prefix[elem[1]+1]-prefix[elem[0]]);
+            if(elem[0]==elem[1] && elem[1]==0)
+            {
+                answer.push_back(0);
+            }
+
+            else if(elem[0]!=elem[1])
+            {
+                if(elem[0]==0) answer.push_back(prefix[elem[1]]);   
+                else answer.push_back(prefix[elem[1]]-prefix[elem[0]-1]);           
+            }
+
+            else{
+                    answer.push_back(prefix[elem[1]]-prefix[elem[0]-1]);
+            }
         }
 
         return answer;
