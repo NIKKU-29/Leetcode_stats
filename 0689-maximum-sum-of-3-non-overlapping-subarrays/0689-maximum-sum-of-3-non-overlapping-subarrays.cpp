9ccdbb03648e5b212a45e55532863cb0f1ec7a66
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-02 20:27:00 +0530

    Time: 0 ms (100%), Space: 27.1 MB (43.75%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -1,40 +1,83 @@
 class Solution {
 public:
+
+    int t[20001][4];
+    int helper(vector<int>&windowSum,int idx, int count , int k)
+    {
+        if(count==0)
+        {
+            return 0;
+        }
+
+        if(idx>=windowSum.size())
+        {
+            return INT_MIN;
+        }
+
+        if(t[idx][count]!=-1)
+        {
+            return t[idx][count];
+        }
+
+
+
+        int take= windowSum[idx] + helper(windowSum,idx+k,count-1,k);
+        int notake=helper(windowSum,idx+1,count,k);
+
+        return t[idx][count]=max(take,notake);
+    }
+
+    void solve(vector<int>&windowSum,int count , int idx,vector<int>& nums, int k,vector<int>&ans)
+    {
+        if(count==0) return;
+
+        if(idx >= windowSum.size())
+        {
+            return;
+        }
+
+        int take = windowSum[idx] + helper(windowSum,idx+k,count-1,k);
+        int notake = helper(windowSum,idx+1,count,k);
+
+
+        if(take >= notake)
+        {
+            ans.push_back(idx);
+            solve(windowSum,count-1,idx+k,nums,k,ans);
+        }
+
+        else{
+            solve(windowSum,count,idx+1,nums,k,ans);
+        }   
+    }
+
+
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
-        const int n=nums.size();
-        vector<int> ksum(n-k+1, 0), L(n-3*k+1, -1);
-        int maxKsum=ksum[0]=accumulate(nums.begin(), nums.begin()+k, 0);
-        L[0]=0;
-        for(int i=1; i<n-k+1; i++){
-            ksum[i]=ksum[i-1]-nums[i-1]+nums[i+k-1];
-            if (i>n-3*k) continue;
-            if (maxKsum<ksum[i]){
-                maxKsum=ksum[i];
-                L[i]=i;
+
+        memset(t , -1, sizeof(t));
+        vector<int>windowSum;
+        int i=0;
+        int j=0;
+        int sum=0;
+
+        while(j < nums.size())
+        {
+            sum+=nums[j];
+
+            if(j-i+1 == k)
+            {
+                windowSum.push_back(sum);
+                sum-=nums[i];
+                i++;
             }
-            else L[i]=L[i-1];
-        //    cout<<i<<"->L->"<<L[i]<<" maxKsum="<<maxKsum<<endl;
+
+            j++;
         }
-        vector<int> R(n-3*k+1, -1);
-        maxKsum=ksum.back();
-        R.back()=n-k;
-        for(int i=n-k-1; i>=2*k; i--){
-            if (maxKsum<=ksum[i]){// multiple answers, return the lexico smallest one
-                maxKsum=ksum[i];
-                R[i-2*k]=i;
-            }
-            else R[i-2*k]=R[i+1-2*k];
-        //    cout<<i-2*k<<"->R->"<<R[i-2*k]<<" maxKsum="<<maxKsum<<endl;
-        }
-        vector<int> ans(3,-1);
-        int max3K=0;
-        for(int i=0; i<=n-3*k; i++){
-             int sum3K=ksum[L[i]]+ksum[i+k]+ksum[R[i]];
-             if (sum3K> max3K){
-                max3K=sum3K;
-                ans={L[i], i+k, R[i]};
-             }
-        }
-        return ans;  
+
+        vector<int>ans;
+        solve(windowSum,3,0,nums,k,ans);
+        return ans;
+
+        
     }
 };
