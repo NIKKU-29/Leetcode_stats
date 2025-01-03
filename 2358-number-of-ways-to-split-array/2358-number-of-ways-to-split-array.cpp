2a83611948427479924c8d403548e8cfa3754d27
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-04 04:15:48 +0530

    Time: 0 ms (100%), Space: 89.4 MB (62.92%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -2,28 +2,50 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
 
-        vector<long long>presum;
-        long long sum=0;
+        // vector<long long>presum;
+        // long long sum=0;
 
+        // for(auto elem : nums)
+        // {
+        //     sum+=elem;
+        //     presum.push_back(sum);
+        // }
+
+        // int count=0;
+        // for(int i=0 ; i< nums.size()-1 ;i++)
+        // {
+        //     long long  sum1=presum[i];
+        //     long long sum2=presum[nums.size() -1]-sum1;
+
+        //     if(sum1 >= sum2)
+        //     {
+        //         count++;
+        //     }
+
+        // }
+
+        // return count;
+
+
+        //to even improve the spae complexity of the code as well as time complexity
+
+        long long totalsum=0;
+        
         for(auto elem : nums)
         {
-            sum+=elem;
-            presum.push_back(sum);
+            totalsum+=elem;
         }
 
-        int count=0;
+        long long suffsum=0;
+        int count=0; 
 
-        for(int i=0 ; i< nums.size()-1 ;i++)
+        for(int i=0 ; i<nums.size()-1 ; i++)
         {
-
-            long long  sum1=presum[i];
-            long long sum2=presum[nums.size() -1]-sum1;
-
-            if(sum1 >= sum2)
+            suffsum+=nums[i];
+            if( suffsum >= totalsum-suffsum )
             {
                 count++;
             }
-
         }
 
         return count;
