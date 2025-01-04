27fd19813a5416f08c4d75df5ad9c5303d32f98c
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-04 19:25:19 +0530

    Time: 594 ms (26.08%), Space: 16.3 MB (38.34%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
 
-         vector<int> left(26, -1);   // Stores the first occurrence index of each character
-        vector<int> right(26, -1);  // Stores the last occurrence index of each character
-        set<pair<char, char>> st;   // Stores unique pairs of (start, middle character)
+        vector<int>left(26,-1);
+        vector<int>right(26,-1);
+        set<pair<char,char>>st;  //stores the visited start and mid element of string
+        
 
-        // Fill the left and right vectors with the positions of characters
-        for (int i = 0; i < s.size(); i++) {
-            if (left[s[i] - 'a'] == -1) left[s[i] - 'a'] = i;
-            right[s[i] - 'a'] = i;
+        for(int i=0 ; i< s.size();i++)
+        {
+            if(left[s[i]-'a']==-1) left[s[i]-'a']=i;
+            right[s[i]-'a']=i;
         }
 
-        // Loop through all characters 'a' to 'z'
-        for (int c = 0; c < 26; c++) {
-            if (left[c] != -1 && right[c] != -1 && left[c] < right[c]) {
-                // If there is a valid range between the leftmost and rightmost positions
-                for (int i = left[c] + 1; i < right[c]; i++) {
-                    // Insert the pair (start, middle) character into the set
-                    st.insert({s[left[c]], s[i]});
+
+        for(int c=0 ; c < 26 ; c++)
+        {
+            if(left[c]!=-1 && right[c]!=-1 && left[c]<right[c])
+            {
+                for(int i=left[c]+1 ; i< right[c] ; i++)
+                {
+                    st.insert({c+'a',s[i]});
                 }
             }
         }
