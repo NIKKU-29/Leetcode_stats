5206816ac69c847b66b4f81d289e71b1dff3ae38
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-06 21:22:50 +0530

    Time: 3 ms (63.6%), Space: 12.1 MB (50.97%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,25 +2,46 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         
-        vector<int>ans;
-        vector<int>relative;
-        for(int i=0 ; i<boxes.size();i++)
-        {
-            if(boxes[i]=='1')
-            {
-                relative.push_back(i);
-            }
-        }
+        // vector<int>ans;
+        // vector<int>relative;
+        // for(int i=0 ; i<boxes.size();i++)
+        // {
+        //     if(boxes[i]=='1')
+        //     {
+        //         relative.push_back(i);
+        //     }
+        // }
 
-        for(int i=0 ; i< boxes.size() ; i++)
-        {
-            int sum=0;
-            for(auto elem : relative)
-            {
-                sum+=abs(i-elem);
-            }
+        // for(int i=0 ; i< boxes.size() ; i++)
+        // {
+        //     int sum=0;
+        //     for(auto elem : relative)
+        //     {
+        //         sum+=abs(i-elem);
+        //     }
             
-            ans.push_back(sum);
+        //     ans.push_back(sum);
+        // }
+
+        // return ans;
+
+        int n=boxes.size();
+        vector<int>ans(n,0);
+        int ballsonleft=0,movesfromleft=0;
+        int ballsonright=0,movesfromright=0;
+
+        for(int i=0 ; i< n ; i++)
+        {
+            ans[i]+=movesfromleft;
+            ballsonleft+=boxes[i]-'0';
+            movesfromleft+=ballsonleft;
+
+            int j=n-i-1;
+            ans[j]+=movesfromright;
+            ballsonright+=boxes[j]-'0';
+            movesfromright+=ballsonright;
+
+
         }
 
         return ans;
