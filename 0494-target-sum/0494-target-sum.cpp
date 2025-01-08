70e4c773c611d80e65b17390e4395e53a444612a
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-08 18:16:50 +0530

    Time: 15 ms (63.11%), Space: 40.9 MB (19.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
 
-    int solver(int idx, int sum , vector<int>& nums, int target )
+    int solver(int idx, int sum , vector<int>& nums, int target,vector<vector<int>>&dp)
     {
         int n=nums.size();
 
-        if(sum==target && idx==n)
+        if(idx==n)
         {
-            return 1;
+           if(sum==target) return 1;
+           return 0;
         }
 
-        else if(idx==n) return 0;
+        
 
-        int plus=solver(idx+1,sum+nums[idx],nums,target);
-        int neg=solver(idx+1,sum-nums[idx],nums,target);
+        if(dp[idx][sum+1000]!=-1)
+        {
+            return dp[idx][sum+1000];
+        }
 
-        return plus+neg;
+        int plus=solver(idx+1,sum+nums[idx],nums,target,dp);
+        int neg=solver(idx+1,sum-nums[idx],nums,target,dp);
+
+        return dp[idx][sum+1000]=plus+neg;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        return solver(0,0,nums,target);
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(2001,-1));
+        return solver(0,0,nums,target,dp);
     }
 };
