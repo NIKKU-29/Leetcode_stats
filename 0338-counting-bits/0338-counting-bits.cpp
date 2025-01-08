348ba499fe559b0ae6ab2b8ae44c585c261e547a
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-08 18:05:05 +0530

    Time: 0 ms (100%), Space: 10.4 MB (69.99%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         
-        vector<int>ans;
+        vector<int>ans(n+1);
         for(int i=0 ; i<= n ; i++)
         {
-            ans.push_back(__builtin_popcount(i));           
+            // ans.push_back(__builtin_popcount(i));       
+            ans[i]=ans[i>>1] + (i%2); 
         }
 
         return ans;
