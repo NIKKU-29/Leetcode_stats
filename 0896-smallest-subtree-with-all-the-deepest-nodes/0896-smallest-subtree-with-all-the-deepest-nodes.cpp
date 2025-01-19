734d5c59e193e2f12dd8f636c9491486f3bcca9e
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-19 19:24:18 +0530

    Time: 0 ms (100%), Space: 16.1 MB (89.28%) - LeetHub

--- 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,21 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    struct Result {
-        TreeNode* node;
-        int depth;
-    };
-    
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        return dfs(root).node;
-    }
-    
-    Result dfs(TreeNode* root) {
-        if (!root) return {nullptr, 0};
-        Result left = dfs(root->left);
-        Result right = dfs(root->right);
+
+    TreeNode* solver(TreeNode* temp,int& depth)
+    {
+        if(!temp) return nullptr;
+
+        int leftdepth=0;
+        int rightdepth=0;
+        TreeNode* left=solver(temp->left,leftdepth);
+        TreeNode* right=solver(temp->right,rightdepth);
+
+        depth=max(leftdepth,rightdepth)+1;
+        //ha level pe depth calulate hogi taki make sure ho sake ki deppest balanced ya unbalanced ans hi return ho
         
-        if (left.depth > right.depth) return {left.node, left.depth + 1};
-        if (left.depth < right.depth) return {right.node, right.depth + 1};
-        return {root, left.depth + 1};
+
+        if(leftdepth==rightdepth) return temp; //balance hone pe node return matlab self return agar left aur right dono side barabar legth hai to hi khud ko return karo
+
+        return leftdepth > rightdepth ? left : right; //otehwise check when hs more depth let or right
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int depth=0;
+        return solver(root,depth);
     }
 };
