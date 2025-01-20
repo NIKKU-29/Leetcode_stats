68b0d8953351eaa54d332389435f55bed36b9d64
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-20 23:40:47 +0530

    Time: 2 ms (77.95%), Space: 7.7 MB (80.46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -4,12 +4,12 @@ public:
 
             if(n==0) return 0;
             if(n==1) return 1;
-            if(n==2) return 1;
+            
 
-            int a=1;
+            int a=0;
             int b=1;
 
-            for(int i=2 ; i<n  ;i++)
+            for(int i=0 ; i<n  ;i++)
             {
                 int temp=a;
                 a=a+b;
