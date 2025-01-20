ec322a5e5449e33374a5726396cc675e7dd2e177
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-21 00:17:09 +0530

    Time: 78 ms (4.86%), Space: 245.7 MB (5.04%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int solver(int idx ,vector<int>cost,vector<int>&dp)
+    {
+        if(idx >= cost.size())
+        {
+            return 0;
+        }
+
+        if(dp[idx]!=-1) return dp[idx];
+
+        int jump1=cost[idx] + solver(idx+1,cost,dp);
+        int jump2=cost[idx] + solver(idx+2,cost,dp);
+
+        return dp[idx]=min(jump1,jump2);
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) { 
+        int n=cost.size();
+        vector<int>dp(n,-1);       
+        return min(solver(0,cost,dp),solver(1,cost,dp));
+    }
+};
