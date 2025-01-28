7332ec12c898bd0e0a31d747c55785fa73fca904
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-28 19:15:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (21.34%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -10,7 +10,15 @@ public:
         {
             prod*=nums[i];
             ans=max(ans,prod);
-            if(prod<0) prod=1;
+            if(prod==0) prod=1;
+        }
+
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=nums[i];
+          ans=max(prod,ans);
+          if(prod==0) prod=1;
         }
 
 
