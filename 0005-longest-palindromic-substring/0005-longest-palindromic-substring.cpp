b7a2ee706a01ef7bbd5ee1b917ec0aec6e00821d
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-29 03:30:08 +0530

    Time: 202 ms (24.94%), Space: 26 MB (43.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,42 +1,89 @@
 class Solution {
 public:
 
-    bool palindrom(int i,int j,string& s)
-    {
-        if(i>=j) 
-        {
-            return true;
-        }
+    // bool palindrom(int i,int j,string& s)
+    // {
+    //     if(i>=j) 
+    //     {
+    //         return true;
+    //     }
 
-        if(s[i]==s[j])
-        {
-            return palindrom(i+1,j-1,s);
-        }
+    //     if(s[i]==s[j])
+    //     {
+    //         return palindrom(i+1,j-1,s);
+    //     }
 
 
-        return false;
-    }
+    //     return false;
+    // }
+
+    // string longestPalindrome(string s) {
+
+    //    int n=s.size();
+    //     int maxL=1;
+    //     int idx=0;
+
+    //     for(int i=0 ;i< n ;i++)
+    //     {
+    //         for(int j=i;j<n;j++)
+    //         {
+    //             if(palindrom(i,j,s))
+    //             {
+    //                 if(j-i+1 > maxL)
+    //                 {
+    //                     maxL=j-i+1;
+    //                     idx=i;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return s.substr(idx,maxL);
+//      }
+//};
 
     string longestPalindrome(string s) {
-
-        int n=s.size();
+        
         int maxL=1;
         int idx=0;
+        int n=s.size();
 
-        for(int i=0 ;i< n ;i++)
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+
+        for(int i=0 ;i< n;i++)
         {
-            for(int j=i;j<n;j++)
+            dp[i][i]=true;
+        }
+
+
+        for(int len=2 ; len<=n;len++)
+        {
+            for(int i=0 ;i<n-len+1;i++)
             {
-                if(palindrom(i,j,s))
+                int j=i+len-1;
+                if(s[i]==s[j] && len==2)
                 {
+                    dp[i][j]=true;
+                    maxL=2;
+                    idx=i;
+                }
+
+                else if(s[i]==s[j] && dp[i+1][j-1])
+                {
+                    dp[i][j]=true;
                     if(j-i+1 > maxL)
-                    {
+                    {    
                         maxL=j-i+1;
                         idx=i;
                     }
                 }
+
+                else{
+                    dp[i][j]=false;
+                }
+
             }
         }
+
         return s.substr(idx,maxL);
     }
 };
