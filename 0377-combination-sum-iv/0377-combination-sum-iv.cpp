07a46c61f11dd4fc403876b8f96905745e439025
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-30 00:12:52 +0530

    Time: 3 ms (41.46%), Space: 9.1 MB (66.31%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int combinationSum4(vector<int>& nums, int target) {
+
+        vector<unsigned int> dp(target + 1, 0);
+        dp[0] = 1; // Base case: one way to make sum 0 (use nothing)
+        
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        
+        return dp[target];
+        
+    }
+};
