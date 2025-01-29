4f48cbcbdffc3e5cbe9882d229db563f2fd3be60
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-01-29 18:42:52 +0530

    Time: 10 ms (13.65%), Space: 14 MB (24.26%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,57 +1,63 @@
 class Solution {
 public:
-    // Main function to find redundant connection
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int> ans;
-        int n = edges.size();
-        vector<vector<int>> adj(n + 1); // Use n+1 since nodes are 1-indexed
-        
-        for(auto elem : edges) {
-            int n1 = elem[0] - 1; // Convert to 0-based indexing for DFS
-            int n2 = elem[1] - 1;
-            
-            if(iscycle(adj, n1, n2)) {
-                ans = elem;
-            }
-            
-            // Add edges to adjacency list (using 1-based indexing)
-            adj[elem[0]].push_back(elem[1]);
-            adj[elem[1]].push_back(elem[0]);
-        }
-        
-        return ans;
-    }
-    
-private:
-    // Helper function to check if adding edge (src, dest) creates a cycle
-    bool iscycle(vector<vector<int>>& adj, int src, int dest) {
-        // If no edges exist yet, no cycle possible
-        if(adj[src + 1].empty() || adj[dest + 1].empty()) 
-            return false;
-            
-        vector<bool> visited(adj.size(), false);
-        return dfs(adj, src + 1, dest + 1, visited, -1);
-    }
-    
-    // DFS function to detect cycle
-    bool dfs(vector<vector<int>>& adj, int curr, int dest, vector<bool>& visited, int parent) {
-        // If current node is destination, we found a path
-        if(curr == dest) 
+
+    bool dfs(int curr,int desti,vector<vector<int>>&adj,int parent,vector<bool>&visited)
+    {
+        if(curr==desti)
+        {
             return true;
-            
-        visited[curr] = true;
-        
-        // Check all neighbors
-        for(int neighbor : adj[curr]) {
-            if(neighbor == parent) // Skip parent node
-                continue;
-                
-            if(!visited[neighbor]) {
-                if(dfs(adj, neighbor, dest, visited, curr))
-                    return true;
+        }
+
+        visited[curr]=true;
+
+        for(auto elem : adj[curr])
+        {
+            if(elem==parent) continue;
+
+            if(!visited[elem])
+            {
+                if(dfs(elem,desti,adj,curr,visited)) return true;
             }
         }
-        
+
         return false;
+
+    }
+
+    bool iscycle(vector<vector<int>>&adj,int n1 ,int n2)
+    {
+        if(adj[n1].empty() || adj[n2].empty()) //node not prsent in graph
+        {
+            return false; 
+        }
+        
+        vector<bool>visited(adj.size(),false);
+        return dfs(n1,n2,adj,-1,visited);
+
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        vector<int>ans;
+        int n=edges.size();
+        vector<vector<int>>adj(n+1);
+
+
+        for(auto elem : edges)
+        {
+            int n1=elem[0];
+            int n2=elem[1];
+
+            if(iscycle(adj,n1,n2)) //if it is possible to reach n2 using n1 neighbourse then cycle is prsent use dfs
+            {
+                ans=elem;
+            }      
+
+            adj[n1].push_back(n2);
+            adj[n2].push_back(n1);       
+        }
+
+        return ans;
+
     }
 };
