c1335273e610913cad5a87aea745e2f8b22ed2f2
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-02 02:21:00 +0530

    Time: 995 ms (30.64%), Space: 11.9 MB (63.54%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
+    bool solver(int k, int idx, int currsum, int target, vector<int>& nums, vector<bool>& visited) {
+        if (k == 0) return true; // If all k subsets are successfully created
+        
+        if (currsum == target) {
+            return solver(k - 1, 0, 0, target, nums, visited); // Start forming next subset
+        }
 
-    bool solver(int k,int idx,int currsum,int target,vector<int>&nums)
-    {
-            if(k==0) return true;
-            if(currsum == target)
-            {
-                return solver(k-1,0,0,target,nums);
-            }
-            
-           
+        for (int i = idx; i < nums.size(); i++) {
+            if (visited[i] || currsum + nums[i] > target) continue; // Skip used elements or exceed target
 
-            for(int i=idx ; i < nums.size() ;i++)
-            {
-                if(nums[i]==-1) continue;
+            // Choose current element
+            visited[i] = true;
+            if (solver(k, i + 1, currsum + nums[i], target, nums, visited)) return true;
+            visited[i] = false; // Backtrack
+        }
 
-                            
-                if(currsum + nums[i] <= target)
-                {
-                    int temp=nums[i];
-                    nums[i]=-1;
-                    if(solver(k,i+1,currsum+temp,target,nums)) return true;
-                    nums[i]=temp;
-
-                }
-                
-            }
-
-            return false;
-            
+        return false;
     }
 
     bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % k != 0) return false; // Cannot be evenly divided
+        
+        int target = sum / k;
+        sort(nums.begin(), nums.end(), greater<int>()); // Optimization
 
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%k!=0) return false;
-        int target=sum/k;
-        sort(nums.begin(),nums.end(),greater<int>());
-        return solver(k,0,0,target,nums);
+        // If the largest element is greater than `target`, it's impossible
+        if (nums[0] > target) return false;
+
+        vector<bool> visited(nums.size(), false);
+        return solver(k, 0, 0, target, nums, visited);
     }
 };
