e645b712c26ea0748ac909f8b06af29c0a7978d3
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-05 04:02:34 +0530

    Time: 0 ms (100%), Space: 50.1 MB (16.42%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int n=nums.size();
-        int ans=0;
-        stack<int>st;
-
-        for(int i=0 ;i<n;i++)
-        {
-            if(st.empty() || nums[st.top()] > nums[i])
-            {
-                st.push(i);
-            }
+        int n = nums.size();
+        vector<int> fromleft(n), fromright(n);
+        
+        // Compute the minimum prefix array
+        fromleft[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            fromleft[i] = min(fromleft[i - 1], nums[i]);
         }
 
-        for(int i=n-1 ; i>=0 ; i--)
-        {
-            while(!st.empty() && nums[st.top()] <= nums[i])
-            {
-                ans=max(ans,i-st.top());
-                st.pop();
-            }
+        // Compute the maximum suffix array
+        fromright[n - 1] = nums[n - 1];
+        for (int j = n - 2; j >= 0; j--) {
+            fromright[j] = max(fromright[j + 1], nums[j]);
         }
 
+        // Use two pointers to find max(j - i)
+        int i = 0, j = 0, ans = 0;
+        while (j < n) {
+            if (fromleft[i] <= fromright[j]) {
+                ans = max(ans, j - i);
+                j++; // Try to maximize width by moving `j`
+            } else {
+                i++; // If condition fails, move `i` forward
+            }
+        }
+        
         return ans;
     }
 };
