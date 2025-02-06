6e1c580aa2a048c11754052b938f52bd69ba1d7b
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-06 12:51:46 +0530

    Time: 310 ms (66.77%), Space: 85 MB (45.45%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        // 1 tupple contribute to ->>8
-        //ab cd -> ab dc -> ba cd -> ba dc
+        // 1 tupple contribute to ->>8 combinations
+        
+        //fix a and b in starting 2
+
+        // [abcd] -> [abcd]-> [bacd] -> [badc]   4 Tuples
+        
+        //ab was in start now cs would be so
+
+        // [cdab] -> [cdba] -> [dcab] -> [dcba]  4 Tuples
+
+
         unordered_map<int,int>mp;
         int n=nums.size();
         for(auto i=0 ;i <n-1 ; i++)
@@ -15,13 +24,23 @@ public:
             }
        }
 
+
+       // if we have three pairs with same product take example of 12 as 1*12=12 
+       // also 2*6=12 and 3*4 =12 we have 3 pairs with same product
+       //now u can select any 2 from these 3 at any instant to make tuple
+
+       // take [1,12,2,6] then take [1,12,3,4] and last take [2,6,3,4]
+       
+       // which make our Valid touple count to 3 no we have 3 tuples
+       // that gives total 24 combinatinons 8 from each  
+
        int count=0;
 
-       for(auto &[key,value] : mp)
+       for(auto &[key,SameProductCount] : mp)
        {
-            if( value > 1)
+            if( SameProductCount > 1)
             {
-                int times= (value * (value -1))/2;
+                int times= (SameProductCount * (SameProductCount -1))/2;
                 count+=(8*times);
             }
        }
