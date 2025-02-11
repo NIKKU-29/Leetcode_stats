fd3a914f8ab21700657fd888fd630f3404f1fbae
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-11 12:33:03 +0530

    Time: 0 ms (100%), Space: 9.5 MB (34.81%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -3,21 +3,11 @@ public:
     string removeOccurrences(string s, string part) {
 
 
-        int n=part.size();
-        string ans="";
-
-        for(auto elem: s){
-
-            ans.push_back(elem);
-
-            if(ans.size() >= n && ans.substr(ans.size()-n) == part)
-            {
-                ans.erase(ans.size()-n);
-            }
-
+        while(s.length()>0 && s.length()>s.find(part)){
+           s.erase(s.find(part),part.length());
         }
-
-        return ans;
+        return s;
+    
         
     }
 };
