79abc62f370d20a94f2b715278f167871391af2a
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-12 22:36:07 +0530

    Time: 8 ms (92.65%), Space: 69.6 MB (41.98%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -2,45 +2,38 @@ class Solution {
 public:
     int maximumSum(vector<int>& nums) {
 
-        int n=nums.size();
-        vector<int>digitsum;
+        int n = nums.size();
+        vector<int> digitsum;
 
-
-        for(auto elem : nums)
-        {
-            int sum=0;
-            while(elem!=0)
-            {
-                sum+=elem % 10;
-                elem=elem/10;
+        for (auto elem : nums) {
+            int sum = 0;
+            while (elem != 0) {
+                sum += elem % 10;
+                elem = elem / 10;
             }
 
             digitsum.push_back(sum);
             // cout<<sum<<" ";
         }
 
-        unordered_map<int,int>mp;
+        vector<int> mp(89,-1);
 
-        int ans=-1;
+        int ans = -1;
 
+        for (int i = 0; i < n; i++) {
+            int sum = digitsum[i];
+            int value = nums[i];
 
-        for(int i=0 ; i< n; i++)
-        {
-            int sum=digitsum[i];
-            int value=nums[i];
-
-            if(!mp.count(sum))
-            {
-                mp[sum]=value;
+            if (mp[sum]==-1) {
+                mp[sum] = value;
             }
 
-            else{
-                    int sumoftwo=mp[sum] + value;
-                    ans=max(ans,sumoftwo);
-                    mp[sum]=max(mp[sum],value);
+            else {
+                int sumoftwo = mp[sum] + value;
+                ans = max(ans, sumoftwo);
+                mp[sum] = max(mp[sum], value);
             }
         }
-        
 
         return ans;
     }
