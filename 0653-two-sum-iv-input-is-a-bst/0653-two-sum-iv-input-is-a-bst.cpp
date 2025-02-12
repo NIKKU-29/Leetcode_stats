7d2c5e32f1d733aaf73a2674a3a4445603d77ffe
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-13 02:08:46 +0530

    Time: 7 ms (28.07%), Space: 37.3 MB (40.22%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -13,24 +13,61 @@ class Solution {
 public:
 
 
-bool solver(TreeNode* root , set<int>&st,int target)
+// bool solver(TreeNode* root , set<int>&st,int target)
+//     {
+//         if(root == nullptr) return false;
+        
+//         int value=(target - root->val);
+        
+//         if(st.count(value))
+//         {
+//             return true;
+//         }
+        
+//         st.insert(root->val);
+//         return solver(root->left,st,target)  || solver(root->right,st,target);
+        
+// //     }
+
+//     bool findTarget(TreeNode* root, int k) {
+//          set<int>st;
+//         return solver(root,st,k);
+//     }
+
+    void inorder(TreeNode* root , vector<int>&vec)
     {
-        if(root == nullptr) return false;
-        
-        int value=(target - root->val);
-        
-        if(st.count(value))
-        {
-            return true;
-        }
-        
-        st.insert(root->val);
-        return solver(root->left,st,target)  || solver(root->right,st,target);
-    
-        
+        if(root==nullptr) return ;
+        inorder(root->left,vec);
+        vec.push_back(root->val);
+        inorder(root->right,vec);
     }
+
+
     bool findTarget(TreeNode* root, int k) {
-         set<int>st;
-        return solver(root,st,k);
+        //2md apporcah building ana array using 2 pointers
+        vector<int>vec;
+        inorder(root,vec);
+
+        int left=0;
+        int right=vec.size()-1;
+
+        while(left < right)
+        {
+            int value = vec[left] + vec[right];
+
+            if(value == k) return true;
+
+           else if(value > k)
+            {
+                right--;
+            }
+
+            else{
+                left++;
+            }
+        }
+
+
+        return false;
     }
 };
