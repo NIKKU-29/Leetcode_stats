def9543761e1c508cdc489e504959d2e227f27e2
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-13 13:32:02 +0530

    Time: 105 ms (23.61%), Space: 99.2 MB (16.98%) - LeetHub

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    typedef long long ll;
+
+    int minOperations(vector<int>& nums, int k) {
+        
+        priority_queue<ll,vector<ll>,greater<ll>>pq;
+
+        for(auto elem : nums) pq.push(elem);
+
+        int count=0;
+
+        while(pq.top() < k && pq.size() >= 2)
+        {
+            ll s1=pq.top();
+            pq.pop();
+            ll s2=pq.top();
+            pq.pop();
+
+            long long value= (min(s1,s2) * 2) + max(s1,s2);
+            count++;
+            pq.push(value);
+            // cout<<value<<" ";
+        }
+
+
+        return count;
+        
+    }
+};
