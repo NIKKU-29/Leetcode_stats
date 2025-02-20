2b8385d39efd44bb5b61e8ea57b5049bac8abf84
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-20 17:08:28 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.88%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,40 +1,45 @@
+// class Solution {
+// public:
+
+//     void solver(int n,string& curr,string& ans,unordered_set<string>&st)
+//     {
+//         if(curr.size() == n)
+//         {
+//             if(!st.count(curr) ans=curr;
+//             return;
+//         }
+
+//         curr+='1';
+//         solver(n,curr,ans,st);
+//         curr.pop_back();
+//         curr+='0';
+//         solver(n,curr,ans,st);
+//         curr.pop_back();
+//         if(!ans.empty()) return;
+//     }
+
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         unordered_set<string>st;
+//         int n=nums.size();
+//         string curr="";
+//         string ans="";
+//         for(auto elem : nums) st.insert(elem);
+//         solver(n,curr,ans,st);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-
-    void solver(int n,string& curr,string& ans,unordered_set<string>&st)
-    {
-        if(curr.size() == n)
-        {
-            if(!st.count(curr))
-            {
-                ans=curr;
-            }
-
-            return;
-        }
-
-        curr+='1';
-        solver(n,curr,ans,st);
-        curr.pop_back();
-        curr+='0';
-        solver(n,curr,ans,st);
-        curr.pop_back();
-        if(!ans.empty()) return;
-    }
-
-    string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string>st;
-        int n=nums.size();
-        string curr="";
+    string findDifferentBinaryString(vector<string>& nums) {    
+        
         string ans="";
 
-        for(auto elem : nums)
+        for(int i=0 ;i< nums.size() ;i++)
         {
-            st.insert(elem);
+            ans += nums[i][i] == '0' ? '1' : '0';
         }
 
-        solver(n,curr,ans,st);
-
-        return ans;
+        return ans;          
     }
 };
