0ed9ae01d372253601723e8fcfcc3df728c77266
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-25 11:49:37 +0530

    Time: 5 ms (49.4%), Space: 117.6 MB (21.75%) - LeetHub

---
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    int mod= 1e9 + 7;
+
+    int numOfSubarrays(vector<int>& arr) {
+
+        int oddcount=0;
+        int evencount=0;
+        int ans=0;
+        int n=arr.size();
+        vector<int>presum(n,0);
+        
+        int sum=0;
+
+        for(int i=0 ;i< n ;i++)
+        {
+            sum+=arr[i];
+            presum[i]=sum;
+        }
+
+        //1 4 9
+
+        for(int i=0 ;i< n ;i++)
+        {
+
+            if(presum[i]%2==1){     //odd
+                 
+                    ans+=1;
+                    oddcount+=1;
+                    ans+= evencount;
+                    ans=ans%mod;
+            }
+
+            else{    
+                    evencount+=1;
+                    ans+= oddcount;
+            }
+        }
+        
+        return ans;
+    }
+};
