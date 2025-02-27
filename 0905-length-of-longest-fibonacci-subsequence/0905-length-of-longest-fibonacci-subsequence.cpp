1328ee536793016d76b2bb31f019bf297462f111
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-27 18:27:15 +0530

    Time: 159 ms (86.11%), Space: 150 MB (21.37%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,35 +1,85 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
-        // Create a map to store value -> index mapping
-        unordered_map<int, int> mp;
-        for(int i = 0; i < n; i++) {
-            mp[arr[i]] = i;
-        }
-        
-        // dp[i][j] represents the length of Fibonacci sequence ending with arr[i] and arr[j]
-        vector<vector<int>> dp(n, vector<int>(n, 2));
-        int maxLen = 0;
-        
-        // For each pair of numbers
-        for(int j = 1; j < n; j++) {
-            for(int i = 0; i < j; i++) {
-                // Current pair: arr[i], arr[j]
-                // We need to find if there's a number arr[k] where k < i
-                // such that arr[k] + arr[i] = arr[j]
-                int diff = arr[j] - arr[i];
-                
-                if(diff < arr[i] && mp.find(diff) != mp.end()) {
-                    int k = mp[diff];
-                    if(k < i) {
-                        dp[i][j] = dp[k][i] + 1;
-                        maxLen = max(maxLen, dp[i][j]);
+
+            int n=arr.size();
+            unordered_map<int,int>mp;
+            for(int i=0 ;i< n ;i++) mp[arr[i]]=i;
+
+
+            vector<vector<int>>dp(n,vector<int>(n,2));
+            int maxans=0;
+
+
+            for(int j=1 ;j<n;j++)
+            {
+                for(int i=0;i<j;i++)
+                {
+                    int value= arr[j] - arr[i];
+
+                    if(value <arr[i] && mp.find(value)!=mp.end())
+                    {
+                        int k=mp[value];
+                        if(k < i)
+                        {
+                            dp[i][j]=dp[k][i] + 1;
+                            maxans=max(maxans,dp[i][j]);
+                        }
                     }
                 }
             }
-        }
-        
-        return maxLen >= 3 ? maxLen : 0;
+
+            return maxans >= 3 ? maxans : 0;
+
+
     }
 };
+
+
+// class Solution {
+// public:
+    
+//     int largest=0;
+
+//     void solver(int idx,int n,vector<int>&curr,vector<int>&arr,unordered_map<int,int>&mp)
+//     {
+//         if(curr.size() >= 3)
+//         {
+//              largest = max(largest, (int)curr.size());
+//         }
+
+//         int prev2=curr[curr.size() - 2];
+//         int prev1=curr[curr.size() - 1];
+//         int sum=prev1+prev2;
+
+//         if(mp.find(sum)!=mp.end())
+//         {
+//             curr.push_back(sum);
+//             solver(mp[sum]+1,n,curr,arr,mp);
+//             curr.pop_back();
+//         }
+
+//         else{
+
+//             return;
+//         }
+
+//     }
+
+//     int lenLongestFibSubseq(vector<int>& arr) {
+
+//             int n=arr.size();
+//             unordered_map<int,int>mp;
+//             for(int i=0 ;i< n ;i++) mp[arr[i]]=i;
+            
+
+//            for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) { 
+//                 vector<int> curr = {arr[i], arr[j]};
+//                 solver(j + 1, n, curr, arr, mp);
+//             }
+//         }
+
+//             return largest;
+//     }
+// };
