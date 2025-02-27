efdc79f830e483f279f5464efc588663af8bda0d
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-02-27 18:01:45 +0530

    Time: 220 ms (83.55%), Space: 149.8 MB (33.97%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,53 +1,31 @@
 class Solution {
 public:
-    
-    int largest=0;
-
-    void solver(int idx,int n,vector<int>&curr,vector<int>&arr,unordered_map<int,int>&mp)
-    {
-        if(curr.size() >= 3)
-        {
-             largest = max(largest, (int)curr.size());
-        }
-
-        // if (idx >= n) {
-           
-        //     return;
-        // }
-
-        int prev2=curr[curr.size() - 2];
-        int prev1=curr[curr.size() - 1];
-        int sum=prev1+prev2;
-
-        if(mp.find(sum)!=mp.end())
-        {
-            curr.push_back(sum);
-            solver(mp[sum]+1,n,curr,arr,mp);
-            curr.pop_back();
-        }
-
-        else{
-
-            return;
-        }
-
-       
-    }
-
     int lenLongestFibSubseq(vector<int>& arr) {
-
-            int n=arr.size();
-            unordered_map<int,int>mp;
-            for(int i=0 ;i< n ;i++) mp[arr[i]]=i;
-            
-
-           for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) { 
-                vector<int> curr = {arr[i], arr[j]};
-                solver(j + 1, n, curr, arr, mp);
+        int n = arr.size();
+        unordered_map<int, int> index;
+        for (int i = 0; i < n; i++) {
+            index[arr[i]] = i;
+        }
+        
+        // dp[i][j] = length of fib subsequence ending with arr[i] and arr[j]
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int result = 0;
+        
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                int prev = arr[j] - arr[i];
+                
+                if (prev < arr[i] && index.count(prev)) {
+                    int k = index[prev];
+                    dp[i][j] = dp[k][i] + 1;
+                } else {
+                    dp[i][j] = 2;
+                }
+                
+                result = max(result, dp[i][j]);
             }
         }
-
-            return largest;
+        
+        return result >= 3 ? result : 0;
     }
 };
