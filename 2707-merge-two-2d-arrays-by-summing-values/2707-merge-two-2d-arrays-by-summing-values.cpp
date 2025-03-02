e11a1289e73363ee0789664de292014dd7682357
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-02 11:00:43 +0530

    Time: 0 ms (100%), Space: 14.7 MB (82.78%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -3,23 +3,49 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         
         // map<int,int>mp;
-        vector<int>mp(1001,0);
+        // vector<int>mp(1001,0);
+        // for(auto elem : nums1) mp[elem[0]]+=elem[1]; 
+        // for(auto elem : nums2) mp[elem[0]]+=elem[1];
+        // vector<vector<int>>ans;
+        // for(int i=0 ;i<= 1000 ;i++) if(mp[i] != 0) ans.push_back({i,mp[i]});  
+        // return ans;
 
-        for(auto elem : nums1)
-        {
-            mp[elem[0]]+=elem[1];
-        }
-
-        for(auto elem : nums2)
-        {
-            mp[elem[0]]+=elem[1];
-        }
-
+        int starta=0,startb=0;
+        int sizea=nums1.size(),sizeb=nums2.size();
         vector<vector<int>>ans;
 
-        for(int i=0 ;i<= 1000 ;i++)
+        while(starta < sizea && startb < sizeb)
         {
-            if(mp[i] != 0) ans.push_back({i,mp[i]});
+            if(nums1[starta][0] > nums2[startb][0])
+            {
+                ans.push_back({nums2[startb][0],nums2[startb][1]});
+                startb++;
+            }
+
+            else if(nums1[starta][0] == nums2[startb][0])
+            {
+                ans.push_back({nums2[startb][0],nums1[starta][1] + nums2[startb][1]});
+                starta++,startb++;
+            }
+
+            else{
+                    ans.push_back({nums1[starta][0],nums1[starta][1]});
+                    starta++;
+            }
+
+        }
+
+
+        while(starta < sizea)
+        {
+            ans.push_back({nums1[starta][0],nums1[starta][1]});
+            starta++;
+        }
+
+        while(startb < sizeb)
+        {
+            ans.push_back({nums2[startb][0],nums2[startb][1]});
+            startb++;
         }
 
         return ans;
