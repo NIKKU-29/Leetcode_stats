b0f87f655cd9f54be769a51e8298b7853543a873
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-03 20:37:10 +0530

    Time: 5 ms (69.33%), Space: 133.4 MB (61.95%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less, equal, greater;
+        
+        deque<int>dq;
+        int n=nums.size();
+        for(int i=0 ; i<n ; i++)
+        {
+            if(nums[i] > pivot)
+            {
+                dq.push_back(nums[i]);
+            }
 
-        for (int num : nums) {
-            if (num < pivot) less.push_back(num);
-            else if (num == pivot) equal.push_back(num);
-            else greater.push_back(num);
+            else if(nums[i] == pivot)
+            {
+                dq.push_front(nums[i]);
+            }
         }
 
-        // Concatenate all parts efficiently
-        vector<int> ans;
-        ans.reserve(nums.size()); // Allocate memory in advance
-        ans.insert(ans.end(), less.begin(), less.end());
-        ans.insert(ans.end(), equal.begin(), equal.end());
-        ans.insert(ans.end(), greater.begin(), greater.end());
-
+        for(int i=n-1 ;i >= 0 ;i--)
+        {
+            if(nums[i] < pivot)
+            {
+                dq.push_front(nums[i]);
+            }
+        }
+        
+        vector<int> ans(dq.begin(), dq.end());
         return ans;
+
+
     }
 };
