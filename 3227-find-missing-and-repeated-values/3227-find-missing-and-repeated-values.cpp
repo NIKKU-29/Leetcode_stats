dbc31e3e5c278b5d9d200c877aea63993900da81
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-06 17:14:59 +0530

    Time: 0 ms (100%), Space: 26.8 MB (77.75%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -6,6 +6,7 @@ public:
             int Sz = n*n;
 
             vector<int>vec(Sz+1);
+            int extra=0;
 
 
             for(int i=0 ;i< n ;i++)
@@ -13,19 +14,18 @@ public:
                 for(int j=0 ;j< n ;j++)
                 {
                     vec[grid[i][j]]++;
+                    if(vec[grid[i][j]] == 2)
+                    {
+                        extra = grid[i][j];
+                    }
                 }
             }
 
-            int extra=0;
+            
             int swapped=0;
             
             for(int i=1 ;i<Sz+1 ;i++)
             {
-                if(vec[i] == 2)
-                {
-                    extra = i;
-                }
-
                 if(vec[i] == 0)
                 {
                     swapped = i;
