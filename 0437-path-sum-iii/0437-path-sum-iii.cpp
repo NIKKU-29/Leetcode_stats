203a3ae1f5252b95f5a6e9280f8c4334fcf5d39d
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-06 22:15:59 +0530

    Time: 608 ms (5.03%), Space: 192.4 MB (5%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -12,26 +12,33 @@
 class Solution {
 public:
 
-    void solver(long long currsum,TreeNode *root, int k , int & count,unordered_map<long long,long long>&mp)
+    int count;
+
+    void solver(long long currsum,TreeNode* root,int k,unordered_map<long long,long long>mp)
     {
-        if(root==nullptr) return;
-        
+        if(!root) return;
+
         currsum+=root->val;
-        count+=mp[currsum-k];
-        
+
+        if(mp.find(currsum - k)!=mp.end())
+        {
+            count+=mp[currsum-k];
+        }
+
         mp[currsum]++;
-        solver(currsum,root->left,k,count,mp);
-        solver(currsum,root->right,k,count,mp);
-        
+
+        solver(currsum,root->left,k,mp);
+        solver(currsum,root->right,k,mp);
+
         mp[currsum]--;
     }
 
 
     int pathSum(TreeNode* root, int targetSum) {
-        int count=0;
+
         unordered_map<long long,long long>mp;
         mp[0]=1;
-        solver(0,root,targetSum,count,mp);
+        solver(0,root,targetSum,mp);
         return count;
     }
 };
