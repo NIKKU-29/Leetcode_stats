4537a049d42299c857e56ce38fb000f05151cb76
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-07 14:37:52 +0530

    Time: 218 ms (42.04%), Space: 14 MB (74.07%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> closestPrimes(int left, int right) {
 
     int n=right; 
-    vector<bool> isPrime(n + 1, true); // Create a boolean array
-    isPrime[0] = isPrime[1] = false;   // 0 and 1 are not prime
+    vector<bool> isPrime(n + 1, true); 
+    isPrime[0] = isPrime[1] = false;   
 
-    for (int i = 2; i * i <= n; i++) { // Loop till sqrt(n)
-        if (isPrime[i]) { // If i is prime
-            for (int j = i * i; j <= n; j += i) { // Mark multiples as false
+    for (int i = 2; i * i <= n; i++) { 
+        if (isPrime[i]) { 
+            for (int j = i * i; j <= n; j += i) { 
                 if(j >= left) isPrime[j] = false;
             }
         }
@@ -33,7 +33,7 @@ public:
 
             if(i - lastPrime < mini)
             {       
-                    cout<<i<<"__"<<lastPrime;
+                    // cout<<i<<"__"<<lastPrime;
                     mini = i - lastPrime;
                     ans={lastPrime,i};
                     
