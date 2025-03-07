d538e6cd63b6e03d85b885370b003672702032f0
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-08 03:24:41 +0530

    Time: 187 ms (5.36%), Space: 74.6 MB (5.48%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-
-    int m,n;
+    int m, n;
 
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
         m = mat.size();
-        n= mat[0].size();
-        
-        queue<pair<int,int>>q;
+        n = mat[0].size();
 
-        for(int  i=0 ;i< m ;i++)
-        {
-            for(int j=0 ; j< n ;j++)
-            {
-                if(mat[i][j] == 0)
-                {
-                    q.push({i,j});
-                }
+        queue<pair<int, int>> q;
 
-                else{
-                    mat[i][j] = -1; //unvisted;
+        // Step 1: Initialize queue and mark unvisited cells
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                } else {
+                    mat[i][j] = -1;  // Mark unvisited
                 }
             }
         }
 
-        vector<vector<int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        // Step 2: BFS traversal
+        vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        while(!q.empty())
-        {
-            int x =q.front().first;
-            int y =q.front().second;
+        while (!q.empty()) {
+            auto [x, y] = q.front();
             q.pop();
 
-            for(auto elem : directions)
-            {
+            for (auto elem : directions) {
                 int newx = x + elem[0];
                 int newy = y + elem[1];
 
-                if(newx < m && newx >=0 && newy < n && newy >=0 && mat[newx][newy] == -1)
-                {
+                if (newx >= 0 && newx < m && newy >= 0 && newy < n && mat[newx][newy] == -1) {
                     mat[newx][newy] = mat[x][y] + 1;
-                    q.push({newx,newy});
+                    q.push({newx, newy});
                 }
             }
         }
