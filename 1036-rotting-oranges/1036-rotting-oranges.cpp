720e242ff7740c8a7f903c6c657353450f4cadee
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-10 01:17:19 +0530

    Time: 0 ms (100%), Space: 16.3 MB (99.95%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,59 +1,64 @@
 class Solution {
 public:
-
     int orangesRotting(vector<vector<int>>& grid) {
-        
-        int orange = 0;
-        int m= grid.size();
+        int m = grid.size();
         int n = grid[0].size();
-        int time=-1;
-        queue<pair<int,int>>q;
+        queue<pair<int, int>> holder;
+        int time = 0;
+        int fresh = 0;
 
-        for(int i=0 ;i<m ;i++)
-        {
-            for(int j=0 ;j< n ;j++)
-            {
-                    if(grid[i][j] ==2)
-                    {
-                        q.push({i, j});
-                    }
-
-                    if(grid[i][j] == 1) orange++;
-            }
-        }
-
-        if(orange == 0 ) return 0;
-
-        vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
-
-        while(!q.empty())
-        {
-            int size = q.size();
-
-            while(size--)
-            {
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-           
-                for(auto elem : directions)
-                {
-                    int newx = x + elem[0];
-                    int newy = y + elem[1];
-
-                    if(newx >= 0 && newx < m && newy >= 0 && newy < n && grid[newx][newy] == 1)
-                    {
-                        q.push({newx,newy});
-                        grid[newx][newy] = 2;
-                        orange--;
-                        
-                    }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    holder.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
                 }
             }
-
-            time++;
         }
 
-        return orange == 0 ? time : -1;
-  }
+        if (fresh == 0) {
+            return 0;
+        }
+
+        while (!holder.empty()) {
+            int change = 0;
+            int size = holder.size();
+
+            while (size) {
+                pair<int, int> elem;
+                elem = holder.front();
+                holder.pop();
+
+                int infx = elem.first;
+                int infy = elem.second;
+
+                int x[4] = {1, -1, 0, 0};
+                int y[4] = {0, 0, 1, -1};
+
+                for (int i = 0; i < 4; i++) {
+                    int sprdx = infx + x[i];
+                    int sprdy = infy + y[i];
+
+                    if (sprdx >= 0 && sprdx < m && sprdy >= 0 && sprdy < n && grid[sprdx][sprdy] == 1) {
+                        holder.push({sprdx, sprdy});
+                        grid[sprdx][sprdy] = 2;
+                        fresh--;
+                        change++;
+                    }
+                }
+                size--;
+            }
+
+            if (change != 0) {
+                time++;
+            }
+        }
+
+        if (fresh != 0) {
+            return -1;
+        }
+
+        return (time == 0) ? -1 : time;
+    }
 };
