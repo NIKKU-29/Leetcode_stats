49f4880d666cab6c09db056fdc8aab0f55f2fc28
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-09 23:57:54 +0530

    Time: 78 ms (29.71%), Space: 102.8 MB (59.21%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -4,15 +4,11 @@ public:
         int i = 0;
         int j = 0;
         int count = 0;
-
-        vector<int>temp(colors.begin(),colors.begin() + (k-1));
-
-        colors.insert(colors.end(),temp.begin(),temp.end());
         int n = colors.size();
 
-        while(j < n)
+        while(j < n + k -1)
         {
-            if(j > 0 && colors[j] == colors[j-1])
+            if(j > 0 && colors[j%n] == colors[(j-1)%n])
             {
                 i = j;
             }
