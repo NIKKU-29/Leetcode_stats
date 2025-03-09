55007b71b3f2b07ec202c06191ea307b6a730a89
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-09 23:59:33 +0530

    Time: 67 ms (74.48%), Space: 102.6 MB (91.84%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        int i = 0, count = 0, n = colors.size();
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int n = colors.size();
 
-        for (int j = 0; j < n + k - 1; j++) {
-            if (j > 0 && colors[j % n] == colors[(j - 1) % n]) {
-                i = j;  // Reset window start if non-alternating pattern is found
+        while(j < n + k -1)
+        {
+            if(j > 0 && colors[j%n] == colors[(j-1)%n])
+            {
+                i = j;
             }
             
-            if (j - i + 1 == k) {  // Check if the window size is exactly k
+            if(j - i + 1 == k)
+            {
                 count++;
-                i++;  // Move start of the window forward to maintain size
+                i++;
             }
-        }
 
-        return count;
+            j++;
+      }
+
+        return count;        
     }
 };
