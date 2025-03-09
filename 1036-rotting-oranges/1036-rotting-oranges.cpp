a10af1c5a87b4b2556f602c08b300101746034ea
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-10 01:16:02 +0530

    Time: 3 ms (30.25%), Space: 18.2 MB (6.17%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
 
-    int solver(int&oranges ,vector<vector<int>>& grid ,queue<pair<int,int>>&q)
-    {
-        int m =grid.size();
-        int n=grid[0].size();
-        int time=0;
+    int orangesRotting(vector<vector<int>>& grid) {
         
+        int orange = 0;
+        int m= grid.size();
+        int n = grid[0].size();
+        int time=-1;
+        queue<pair<int,int>>q;
+
+        for(int i=0 ;i<m ;i++)
+        {
+            for(int j=0 ;j< n ;j++)
+            {
+                    if(grid[i][j] ==2)
+                    {
+                        q.push({i, j});
+                    }
+
+                    if(grid[i][j] == 1) orange++;
+            }
+        }
+
+        if(orange == 0 ) return 0;
+
         vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
 
         while(!q.empty())
@@ -28,7 +45,7 @@ public:
                     {
                         q.push({newx,newy});
                         grid[newx][newy] = 2;
-                        oranges--;
+                        orange--;
                         
                     }
                 }
@@ -37,31 +54,6 @@ public:
             time++;
         }
 
-        return oranges == 0 ? time-1 : -1;
-    }
-
-    int orangesRotting(vector<vector<int>>& grid) {
-        
-        int orange = 0;
-        int m= grid.size();
-        int n = grid[0].size();
-        queue<pair<int,int>>q;
-
-        for(int i=0 ;i<m ;i++)
-        {
-            for(int j=0 ;j< n ;j++)
-            {
-                    if(grid[i][j] ==2)
-                    {
-                        q.push({i, j});
-                    }
-
-                    if(grid[i][j] == 1) orange++;
-            }
-        }
-
-        if(orange == 0 ) return 0;
-
-        return solver(orange,grid,q);
+        return orange == 0 ? time : -1;
   }
 };
