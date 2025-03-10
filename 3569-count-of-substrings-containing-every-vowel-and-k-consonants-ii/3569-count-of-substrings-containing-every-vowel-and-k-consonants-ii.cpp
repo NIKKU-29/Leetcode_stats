226b33fb911f58d2d52856a5f19f68074f93a78b
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-11 03:36:43 +0530

    Time: 204 ms (44.38%), Space: 48.7 MB (59.55%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,3 +1,69 @@
+// class Solution {
+// public:
+
+//     bool isVowel(char c) { 
+//         return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+//     }
+
+//     long long countOfSubstrings(string word, int k) {
+//         int n=word.size();
+//         int i=0,j=0;
+//         unordered_map<char,int>mp;
+//         int C=0;
+//         long long ans=0;
+
+//         vector<int>NextConsonant(n);
+//         int maxic = n;
+
+//         for(int i= n-1 ; i>=0 ; i--)
+//         {
+//             NextConsonant[i]=maxic;
+//             if(!isVowel(word[i])) maxic = i;
+//         }
+
+//         while(j < n)
+//         {
+//             if (isVowel(word[j])) mp[word[j]]++;
+//             else C++;
+
+//             while(C > k)
+//             {
+//                 char ch = word[i];
+//                  if (isVowel(ch))
+//                 {   
+//                     mp[ch]--;
+//                     if(mp[ch] == 0) mp.erase(ch);
+//                 }
+
+//                 else C--;  
+
+//                 i++; 
+//             }
+
+//             while(i < n && mp.size() == 5 && C == k)
+//             {
+//                 int nxtidx = NextConsonant[j];
+//                 ans+= nxtidx - j;
+
+//                 if (isVowel(word[i]))
+//                 {   
+//                     mp[word[i]]--;
+//                     if(mp[word[i]] == 0) mp.erase(word[i]);
+//                 }
+//                 else C--;
+
+//                 i++;
+//             }
+
+//             j++;
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
 
@@ -5,45 +71,24 @@ public:
         return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     }
 
-    long long countOfSubstrings(string word, int k) {
-        int n=word.size();
+
+    long long solver(string word , int k)
+    {
         int i=0,j=0;
+        int n = word.size();
+        long long ans =0;
         unordered_map<char,int>mp;
-        int C=0;
-        long long ans=0;
-
-        vector<int>NextConsonant(n);
-        int maxic = n;
-
-        for(int i= n-1 ; i>=0 ; i--)
-        {
-            NextConsonant[i]=maxic;
-            if(!isVowel(word[i])) maxic = i;
-        }
+        int C =0;
 
         while(j < n)
-        {
-            if (isVowel(word[j])) mp[word[j]]++;
+        {   
+            char ch = word[j];
+            if(isVowel(ch)) mp[ch]++;
             else C++;
 
-            while(C > k)
+            while(mp.size() == 5 && C >= k)
             {
-                char ch = word[i];
-                 if (isVowel(ch))
-                {   
-                    mp[ch]--;
-                    if(mp[ch] == 0) mp.erase(ch);
-                }
-
-                else C--;  
-
-                i++; 
-            }
-
-            while(i < n && mp.size() == 5 && C == k)
-            {
-                int nxtidx = NextConsonant[j];
-                ans+= nxtidx - j;
+                ans+= n - j;
 
                 if (isVowel(word[i]))
                 {   
@@ -55,9 +100,15 @@ public:
                 i++;
             }
 
+
             j++;
         }
 
         return ans;
+    }
+
+    long long countOfSubstrings(string word, int k) {
+        
+        return solver(word, k) - solver(word , k+1);
     }
 };
