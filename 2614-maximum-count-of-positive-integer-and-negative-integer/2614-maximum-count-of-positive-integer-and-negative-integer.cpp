88be3aac3889ce6a1b6cf0eaf0387dcdf7d42bcb
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-12 14:41:47 +0530

    Time: 0 ms (100%), Space: 22.7 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,9 +1,66 @@
 class Solution {
 public:
+
+    int low(vector<int>nums)
+    {
+        int n = nums.size();
+        int l=0;
+        int r=n-1;
+        int idx = n;
+
+        while(l <= r)
+        {
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] < 0)
+            {
+                l = mid + 1;
+            }
+
+            else if(nums[mid] >= 0)
+            {
+                r = mid - 1;
+                idx = mid;
+            }
+        }
+
+        return idx;
+    }
+
+    int high(vector<int>nums)
+    {
+        int n = nums.size();
+        int l=0;
+        int r=n-1;
+        int idx = n;
+
+        while(l <= r)
+        {
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] <= 0)
+            {
+                l = mid + 1;
+            
+            }
+
+            else if(nums[mid] > 0)
+            {
+                r = mid - 1;
+                idx=mid;
+            }
+        }
+
+        return idx;
+    }
+
     int maximumCount(vector<int>& nums) {
         int n=nums.size();
-        int L = lower_bound(nums.begin(),nums.end(),0) - nums.begin();
-        int R = upper_bound(nums.begin(),nums.end(),0) - nums.begin();
+        // int L = lower_bound(nums.begin(),nums.end(),0) - nums.begin();
+        // int R = upper_bound(nums.begin(),nums.end(),0) - nums.begin();
+
+        int L = low(nums);
+        int R = high(nums);
         return max((n - R),(L));
     }
 };
