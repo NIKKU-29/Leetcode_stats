736dcf45e2f582e44815c96f0a0f5566c7490bf4
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-19 17:54:22 +0530

    Time: 91 ms (41.12%), Space: 174.3 MB (16.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,89 +1,32 @@
 class Solution {
 public:
+    string expand(int i, int j, string s) {
+        int left = i;
+        int right = j;
 
-    // bool palindrom(int i,int j,string& s)
-    // {
-    //     if(i>=j) 
-    //     {
-    //         return true;
-    //     }
-
-    //     if(s[i]==s[j])
-    //     {
-    //         return palindrom(i+1,j-1,s);
-    //     }
-
-
-    //     return false;
-    // }
-
-    // string longestPalindrome(string s) {
-
-    //    int n=s.size();
-    //     int maxL=1;
-    //     int idx=0;
-
-    //     for(int i=0 ;i< n ;i++)
-    //     {
-    //         for(int j=i;j<n;j++)
-    //         {
-    //             if(palindrom(i,j,s))
-    //             {
-    //                 if(j-i+1 > maxL)
-    //                 {
-    //                     maxL=j-i+1;
-    //                     idx=i;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return s.substr(idx,maxL);
-//      }
-//};
-
-    string longestPalindrome(string s) {
-        
-        int maxL=1;
-        int idx=0;
-        int n=s.size();
-
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-
-        for(int i=0 ;i< n;i++)
-        {
-            dp[i][i]=true;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
         }
 
+        return s.substr(left + 1, right - left - 1);
+    }
 
-        for(int len=2 ; len<=n;len++)
-        {
-            for(int i=0 ;i<n-len+1;i++)
-            {
-                int j=i+len-1;
-                if(s[i]==s[j] && len==2)
-                {
-                    dp[i][j]=true;
-                    maxL=2;
-                    idx=i;
-                }
+    string longestPalindrome(string s) {
+        string ans = "";
 
-                else if(s[i]==s[j] && dp[i+1][j-1])
-                {
-                    dp[i][j]=true;
-                    if(j-i+1 > maxL)
-                    {    
-                        maxL=j-i+1;
-                        idx=i;
-                    }
-                }
+        for (int i = 0; i < s.size(); i++) {
+            string odd = expand(i, i, s);
+            if (odd.size() > ans.size()) {
+                ans = odd;
+            }
 
-                else{
-                    dp[i][j]=false;
-                }
-
+            string even = expand(i, i + 1, s);
+            if (even.size() > ans.size()) {
+                ans = even;
             }
         }
 
-        return s.substr(idx,maxL);
+        return ans;
     }
 };
