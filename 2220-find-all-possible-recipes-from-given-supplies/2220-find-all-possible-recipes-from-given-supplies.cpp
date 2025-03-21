b1a972d46bb90539a06461c9498527c1692d45d7
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-21 22:25:03 +0530

    Time: 672 ms (7.92%), Space: 99.3 MB (97.73%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,57 +1,47 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, int> Available;
+        vector<bool> done(recipes.size(), false);  // Track which recipes are already made
+        
+        // Mark all initial supplies as available
+        for (auto& elem : supplies) Available[elem]++;
+        
+        int count = recipes.size();  // Number of recipes to process
+        while (count--) {
+            bool madeRecipe = false;  // Track if at least one recipe is made in this iteration
 
-            unordered_map<string,int>Available;
+            for (int i = 0; i < recipes.size(); i++) {
+                if (done[i]) continue;  // Skip recipes that are already made
 
-            for(auto elem : supplies) Available[elem]++;
-
-            int count = recipes.size();
-
-            while(count--)
-            {
-
-                for(int i =0 ;i < ingredients.size() ;i++)
-                {
-
-                    int n = ingredients[i].size();
-                    bool outlier = false;
-                    for(int j=0 ; j< n ; j++)
-                    {
-                        if(Available.find(ingredients[i][j]) == Available.end())
-                        {
-                            outlier = true;
-                            break;
-                        }   
-                    }
-
-                    if(outlier == false)
-                    {
-                        Available[recipes[i]]++;
-                    }
-
-                }
-            }
-
-
-
-            vector<string>ans;
-
-            for(int i =0 ;i< recipes.size() ; i++)
-            {
                 int n = ingredients[i].size();
                 bool outlier = false;
-                for(int j=0 ; j< n ; j++)
-                {
-                    if(Available.find(ingredients[i][j]) == Available.end())
-                    {
+                
+                // Check if all ingredients are available
+                for (int j = 0; j < n; j++) {
+                    if (Available.find(ingredients[i][j]) == Available.end()) {
                         outlier = true;
                         break;
-                    }   
+                    }
                 }
 
-                if(!outlier) ans.push_back(recipes[i]);
+                // If all ingredients are available, mark recipe as done
+                if (!outlier) {
+                    Available[recipes[i]]++;
+                    done[i] = true;  // Mark this recipe as made
+                    madeRecipe = true;
+                }
             }
+
+            // If no new recipe was made in this iteration, break early
+            if (!madeRecipe) break;
+        }
+
+        // Collect all successfully made recipes
+        vector<string> ans;
+        for (int i = 0; i < recipes.size(); i++) {
+            if (done[i]) ans.push_back(recipes[i]);
+        }
 
         return ans;
     }
