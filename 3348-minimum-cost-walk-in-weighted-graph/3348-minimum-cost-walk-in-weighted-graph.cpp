fc90ed0b9b05d2c5dea2ca9ea76c3193aa36f322
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-21 21:25:59 +0530

    Time: 11 ms (93.37%), Space: 206.1 MB (70.22%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,74 +1,84 @@
+class Disjoint {
+    vector<int> parent, rank, cost;
+
+public:
+    Disjoint(int n, vector<int>& InitialCost) {
+        rank.resize(n, 0);
+        parent.resize(n);
+        cost = InitialCost;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Path Compression
+        }
+        return parent[x];
+    }
+
+    void Union(int x, int y, int w) {
+        int parent_x = find(x);
+        int parent_y = find(y);
+
+        if (parent_x == parent_y) {
+            cost[parent_x] &= w;  // Update cost even for same set
+            return;
+        }
+
+        if (rank[parent_x] > rank[parent_y]) {
+            parent[parent_y] = parent_x;
+            cost[parent_x] &= (cost[parent_y] & w);
+        } 
+        else if (rank[parent_y] > rank[parent_x]) {
+            parent[parent_x] = parent_y;
+            cost[parent_y] &= (cost[parent_x] & w);
+        } 
+        else {
+            parent[parent_x] = parent_y;
+            cost[parent_y] &= (cost[parent_x] & w);
+            rank[parent_y]++;
+        }
+    }
+
+    int getcost(int x) {
+        return cost[find(x)];
+    }
+};
+
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> depth;
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        vector<int> InitialCost(n, INT_MAX);
+        Disjoint ds(n, InitialCost);
 
-    vector<int> minimumCost(int n, vector<vector<int>>& edges,
-                            vector<vector<int>>& queries) {
-        // Initialize the parent array with -1 as initially each node belongs to
-        // its own component
-        parent.resize(n, -1);
-        depth.resize(n, 0);
-
-        // All values are initially set to the number with only 1s in its binary
-        // representation
-        vector<unsigned int> componentCost(n, -1);
-
-        // Construct the connected components of the graph
-        for (auto& edge : edges) {
-            Union(edge[0], edge[1]);
+        for (auto& elem : edges) {
+            int x = elem[0];
+            int y = elem[1];
+            int w = elem[2];
+            ds.Union(x, y, w);
         }
 
-        // Calculate the cost of each component by performing bitwise AND of all
-        // edge weights in it
-        for (auto& edge : edges) {
-            int root = find(edge[0]);
-            componentCost[root] &= edge[2];
-        }
+        vector<int> ans;
+        for (auto& q : query) {
+            int x = q[0];
+            int y = q[1];
 
-        vector<int> answer;
-        for (auto& query : queries) {
-            int start = query[0];
-            int end = query[1];
+            int parent_x = ds.find(x);
+            int parent_y = ds.find(y);
 
-            // If the two nodes are in different connected components, return -1
-            if (find(start) != find(end)) {
-                answer.push_back(-1);
-            } else {
-                // Find the root of the edge's component
-                int root = find(start);
-                // Return the precomputed cost of the component
-                answer.push_back(componentCost[root]);
+            if (x == y) {
+                ans.push_back(0);
+            }
+            else if (parent_x != parent_y) {
+                ans.push_back(-1);
+            }
+            else {
+                int cost = ds.getcost(parent_x);
+                ans.push_back(cost);
             }
         }
-        return answer;
-    }
-
-private:
-    // Find function to return the root (representative) of a node's component
-    int find(int node) {
-        // If the node is its own parent, it is the root of the component
-        if (parent[node] == -1) return node;
-        // Otherwise, recursively find the root and apply path compression
-        return parent[node] = find(parent[node]);
-    }
-
-    // Union function to merge the components of two nodes
-    void Union(int node1, int node2) {
-        int root1 = find(node1);
-        int root2 = find(node2);
-
-        // If the two nodes are already in the same component, do nothing
-        if (root1 == root2) return;
-
-        // Union by depth: ensure the root of the deeper tree becomes the parent
-        if (depth[root1] < depth[root2]) swap(root1, root2);
-
-        // Merge the two components by making root1 the parent of root2
-        parent[root2] = root1;
-
-        // If both components had the same depth, increase the depth of the new
-        // root
-        if (depth[root1] == depth[root2]) depth[root1]++;
+        return ans;
     }
 };
