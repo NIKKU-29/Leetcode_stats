a523f6f19d7e9295ea935363995c82c37e26df3c
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-21 21:27:14 +0530

    Time: 24 ms (62.69%), Space: 206.3 MB (70.02%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -6,6 +6,7 @@ public:
         rank.resize(n, 0);
         parent.resize(n);
         cost = InitialCost;
+
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
@@ -22,63 +23,78 @@ public:
         int parent_x = find(x);
         int parent_y = find(y);
 
+
         if (parent_x == parent_y) {
-            cost[parent_x] &= w;  // Update cost even for same set
+            cost[parent_x] &= w;  
             return;
         }
 
+        else{
+
         if (rank[parent_x] > rank[parent_y]) {
             parent[parent_y] = parent_x;
-            cost[parent_x] &= (cost[parent_y] & w);
+            cost[parent_x] &= cost[parent_y];
+            cost[parent_x] &=w;  // Apply AND operation
         } 
         else if (rank[parent_y] > rank[parent_x]) {
             parent[parent_x] = parent_y;
-            cost[parent_y] &= (cost[parent_x] & w);
+            cost[parent_y] &= cost[parent_x];
+            cost[parent_y] &=w;  
         } 
         else {
             parent[parent_x] = parent_y;
-            cost[parent_y] &= (cost[parent_x] & w);
+            cost[parent_y] &= cost[parent_x];
+            cost[parent_y] &=w;  
             rank[parent_y]++;
         }
     }
+        
+
+    }
 
     int getcost(int x) {
-        return cost[find(x)];
+        return cost[find(x)];  // Get cost of representative
     }
 };
 
 class Solution {
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<int> InitialCost(n, INT_MAX);
+        vector<int> InitialCost(n, INT_MAX);  // Use INT_MAX for bitwise AND operations
+
         Disjoint ds(n, InitialCost);
 
         for (auto& elem : edges) {
             int x = elem[0];
             int y = elem[1];
             int w = elem[2];
+
             ds.Union(x, y, w);
         }
 
         vector<int> ans;
+
         for (auto& q : query) {
             int x = q[0];
             int y = q[1];
 
+
             int parent_x = ds.find(x);
             int parent_y = ds.find(y);
 
-            if (x == y) {
-                ans.push_back(0);
-            }
-            else if (parent_x != parent_y) {
+
+            if(x == y) ans.push_back(0);
+
+            else if ( parent_x != parent_y) {
                 ans.push_back(-1);
-            }
+            } 
+
             else {
                 int cost = ds.getcost(parent_x);
                 ans.push_back(cost);
             }
         }
+
         return ans;
     }
 };
