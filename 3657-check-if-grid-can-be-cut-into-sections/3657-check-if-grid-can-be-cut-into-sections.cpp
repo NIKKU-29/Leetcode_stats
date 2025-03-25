3ded758281fe7e1afa81364e2266efb4e3a99cbd
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-25 22:11:51 +0530

    Time: 60 ms (96.28%), Space: 203.5 MB (80.96%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        return canCut(rectangles, 0) || canCut(rectangles, 1);
+    }
+    
+private:
+    bool canCut(vector<vector<int>>& rectangles, int axis) {
+        vector<pair<int,int>>intervals;
+
+        for (const auto& rect : rectangles) {
+            int xy1 = rect[axis];    
+            int xy2 = rect[axis + 2]; 
+            intervals.push_back({xy1,xy2});
+        }
+
+       sort(intervals.begin(),intervals.end());
+       vector<pair<int,int>>vec;
+       
+       for(auto elem : intervals)
+       {
+            if(vec.empty())
+            {
+                vec.push_back(elem);
+            }
+
+            else{
+                    int x = elem.first;
+                    int y = elem.second;
+
+                    if(vec.back().second > x)
+                    {
+                        int newx = min(vec.back().first,x);
+                        int newy = max(vec.back().second,y);
+                        vec.pop_back();
+                        vec.push_back({newx,newy});
+                    }
+
+                    else{
+                        vec.push_back({x,y});
+                    }
+
+            }
+       }
+        
+        if(vec.size() >= 3) return true; 
+        
+        return false;
+    }
+};
