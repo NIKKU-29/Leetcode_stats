dde7746ac3fb3f1d69eae4f09b72fc1bf8cf5267
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-03-27 00:36:27 +0530

    Time: 400 ms (40.92%), Space: 211.6 MB (35.43%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    int solver(int idx, int mode, int txnCount, vector<int>& prices, vector<vector<vector<int>>>& dp) {
-        if (idx == prices.size() || txnCount == 2) return 0; // Max 2 transactions
-
-        if (dp[idx][mode][txnCount] != -1) return dp[idx][mode][txnCount];
-
-        int profit = 0;
-        if (mode == 0) { // Buy Mode
-            int buy = -prices[idx] + solver(idx+1, 1, txnCount, prices, dp);
-            int notBuy = solver(idx+1, 0, txnCount, prices, dp);
-            profit = max(buy, notBuy);
-        } else { // Sell Mode
-            int sell = prices[idx] + solver(idx+1, 0, txnCount+1, prices, dp);
-            int notSell = solver(idx+1, 1, txnCount, prices, dp);
-            profit = max(sell, notSell);
-        }
-
-        return dp[idx][mode][txnCount] = profit;
-    }
+   
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
-        return solver(0, 0, 0, prices, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));;
+        
+
+         for(int idx = 0 ;idx <= n ;idx++)
+        {
+            dp[idx][0][2] = 0;
+            dp[idx][1][2] = 0;
+        }
+
+        for(int txnCount = 0 ;txnCount <= 2 ; txnCount++)
+        {   
+            dp[n][0][txnCount] = 0;
+            dp[n][1][txnCount] = 0;
+        }
+        
+
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int mode = 0; mode <= 1; mode++) {
+                for (int txnCount = 1; txnCount <= 2; txnCount++) {
+
+                    int profit=0;
+
+                     if (mode == 0) { // Buy Mode
+                    int buy = -prices[idx] + dp[idx+1][1][txnCount];
+                    int notBuy = dp[idx+1][0][txnCount];
+                    profit = max(buy, notBuy);
+                    }
+
+                else { // Sell Mode
+                    int sell = prices[idx] + dp[idx+1][0][txnCount-1];
+                    int notSell = dp[idx+1][1][txnCount];
+                    profit = max(sell, notSell);
+                }
+
+                    dp[idx][mode][txnCount] = profit;
+                }
+            }
+        }
+
+
+        return dp[0][0][2];
     }
 };
