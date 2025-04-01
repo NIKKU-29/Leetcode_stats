4878dffb9239e549874e50a9a2c402181ee485b0
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-01 21:33:45 +0530

    Time: 8 ms (49.73%), Space: 119.8 MB (58.89%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,21 +1,44 @@
+// class Solution {
+// public:
+
+//     long long solver(int idx ,vector<vector<int>>&questions,vector<long long>&dp)
+//     {
+//         if(idx >= questions.size()) return 0;
+
+//         if(dp[idx]!= -1) return dp[idx];
+
+//         long long take = questions[idx][0] + solver(idx+questions[idx][1] + 1 , questions,dp);
+//         long long notake = solver(idx+1,questions,dp);
+
+//         return dp[idx] = max(take , notake);
+
+//     }
+
+//     long long mostPoints(vector<vector<int>>& questions) {
+//         vector<long long>dp(questions.size(),-1);
+//         return solver(0,questions,dp);
+//     }
+// };
+
+
 class Solution {
 public:
-
-    long long solver(int idx ,vector<vector<int>>&questions,vector<long long>&dp)
-    {
-        if(idx >= questions.size()) return 0;
-
-        if(dp[idx]!= -1) return dp[idx];
-
-        long long take = questions[idx][0] + solver(idx+questions[idx][1] + 1 , questions,dp);
-        long long notake = solver(idx+1,questions,dp);
-
-        return dp[idx] = max(take , notake);
-
-    }
-
     long long mostPoints(vector<vector<int>>& questions) {
-        vector<long long>dp(questions.size(),-1);
-        return solver(0,questions,dp);
+            int n = questions.size();
+            vector<long long>dp(n+1,0);
+           
+
+           for(int idx = n-1; idx>=0 ;idx--)
+           {
+
+                long long take = questions[idx][0] + (idx + questions[idx][1] + 1 < n ? dp[idx + questions[idx][1] + 1] : 0);
+                long long notake = dp[idx+1];
+
+                 dp[idx] = max(take , notake);              
+           }
+
+
+           return dp[0];
+
     }
 };
