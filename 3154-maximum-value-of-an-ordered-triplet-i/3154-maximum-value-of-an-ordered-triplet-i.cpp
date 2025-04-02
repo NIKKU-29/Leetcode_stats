2b7c08a7f47dce866c50fc055c0101bbbd575e02
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-02 18:19:58 +0530

    Time: 0 ms (100%), Space: 21.6 MB (88.55%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -9,8 +9,8 @@ public:
         for(long long elem : nums)
         {
             max3= max(max3,maxdiff * elem);
-            maxSofar = max(maxSofar,elem);
             maxdiff = max(maxdiff,maxSofar - elem);
+            maxSofar = max(maxSofar,elem);
         }
 
         return max3;
