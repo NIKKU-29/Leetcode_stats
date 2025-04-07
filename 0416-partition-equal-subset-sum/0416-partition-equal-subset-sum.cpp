1319f6e85fe4c57382dba260d2eece73019d3b88
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-07 18:28:59 +0530

    Time: 210 ms (10.69%), Space: 186.2 MB (8.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<vector<int>> dp;
 
-    bool solver(int idx, int target, vector<int>& nums) {
+    bool solver(int idx, int target, vector<int>& nums,vector<vector<int>>&dp) {
         if (target == 0) return true;
         if (idx == nums.size() || target < 0) return false;
         if (dp[idx][target] != -1) return dp[idx][target];
 
-        bool take = solver(idx + 1, target - nums[idx], nums);
-        bool notake = solver(idx + 1, target, nums);
+        bool take = solver(idx + 1, target - nums[idx], nums,dp);
+        bool notake = solver(idx + 1, target, nums,dp);
 
         return dp[idx][target] = take || notake;
     }
@@ -16,9 +16,10 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum % 2 != 0) return false;
+        int n = nums.size();
         int target = sum / 2;
-
-        dp = vector<vector<int>>(nums.size(), vector<int>(target + 1, -1));
-        return solver(0, target, nums);
+           vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        
+        return solver(0, target, nums,dp);
     }
 };
