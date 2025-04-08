9dff55be87fdb912a6cd33855d50034ad0585236
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-08 06:37:19 +0530

    Time: 7 ms (32.42%), Space: 28.4 MB (29.69%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        map<int,int>mp;
-        int n=nums.size();
-        int elemcount=n;
+        int n =nums.size();
+        unordered_map<int,int>mp;
+        int Ecount = n;
+
         for(auto elem : nums)
-            {mp[elem]++;}
+        {
+            mp[elem]++;
+        }
 
-
-        int count=0; //number of times removing starting 3 is done
-        for(int i=0 ; i < n ;i+=3)
+        int count =0;
+        
+        for(int i = 0 ; i<  n ;i+=3)
+        {
+            if(mp.size() != Ecount)
             {
-                if(mp.size()!=elemcount)
-                {
-                    if(mp.size() < 3) 
-                    {
 
-                        count++;
-                        return count;
-                    }
-                    
-                    //remove starting 3
-                    mp[nums[i]]--;
-                    if(mp[nums[i]]==0) mp.erase(nums[i]);
-                    mp[nums[i+1]]--;
-                    if(mp[nums[i+1]]==0) mp.erase(nums[i+1]);
-                    mp[nums[i+2]]--;
-                    if(mp[nums[i+2]]==0) mp.erase(nums[i+2]);
-                    elemcount-=3;
+                if(Ecount < 3)
+                {
                     count++;
+                    return count;
                 }
-                    
-                else{
-                        return count;
-                }
+
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                mp[nums[i+1]]--;
+                if(mp[nums[i+1]]==0) mp.erase(nums[i+1]);
+                mp[nums[i+2]]--;
+                if(mp[nums[i+2]]==0) mp.erase(nums[i+2]);
+
+                Ecount-=3;
+                count++;
             }
+
+            else{
+                    return count;
+            }
+        }
+
 
         return count;
     }
