d94607408aa224e2e0480ff9a3a6b87d58862678
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-08 06:37:43 +0530

    Time: 41 ms (6.66%), Space: 40.9 MB (5.63%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int n =nums.size();
-        unordered_map<int,int>mp;
-        int Ecount = n;
-
-        for(auto elem : nums)
-        {
-            mp[elem]++;
-        }
-
-        int count =0;
-        
-        for(int i = 0 ; i<  n ;i+=3)
-        {
-            if(mp.size() != Ecount)
-            {
-
-                if(Ecount < 3)
-                {
-                    count++;
-                    return count;
-                }
-
-                mp[nums[i]]--;
-                if(mp[nums[i]]==0) mp.erase(nums[i]);
-                mp[nums[i+1]]--;
-                if(mp[nums[i+1]]==0) mp.erase(nums[i+1]);
-                mp[nums[i+2]]--;
-                if(mp[nums[i+2]]==0) mp.erase(nums[i+2]);
-
-                Ecount-=3;
-                count++;
+        int count = 0;
+        while (true) {
+            set<int> st(nums.begin(), nums.end());
+            if (st.size() == nums.size()) {
+                break; // All elements are unique now
             }
 
-            else{
-                    return count;
-            }
+            // Remove first 3 elements or all if < 3
+            int k = min(3, (int)nums.size());
+            nums.erase(nums.begin(), nums.begin() + k);
+            count++;
         }
-
-
         return count;
     }
 };
