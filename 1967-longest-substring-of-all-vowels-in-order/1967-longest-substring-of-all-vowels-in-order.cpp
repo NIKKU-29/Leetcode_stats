d3666ae285c33f998639d758ded5043158b6ad85
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-11 00:13:37 +0530

    Time: 7 ms (86.47%), Space: 29.4 MB (73.82%) - LeetHub

--- 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.cpp
+++ 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.cpp
@@ -10,19 +10,24 @@ public:
         int n = word.size();
         int start = 0;
         int maxlen = 0;
-        unordered_map<char,int>mp;
+        // unordered_map<char,int>mp;
+        int diff = 1;
+        // while(end > n)
 
         for(int end = 0 ; end < n ; end ++)
         {
             if(end > 0 && word[end] < word[end -1])
             {
+                diff = 1;
                 start = end;
-                mp.clear();
+                // mp.clear();
             }
 
-            mp[word[end]]++;
+            
 
-            if(mp.size() == 5)
+            else if(end > 0 && word[end] > word[end -1]) diff++;
+
+            if(diff == 5)
             {
                 maxlen = max(maxlen,end - start + 1);
             }
