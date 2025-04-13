ae4b6179439f2e9066a310f7294d25be7823ce96
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-14 01:57:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (69.95%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    long long power(long long base, long long exp) {
+        long long result = 1;
+        base %= MOD;
+
+        while (exp > 0) {
+            if (exp % 2 == 1)
+                result = (result * base) % MOD;
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long e = (n + 1) / 2; // ceil(n / 2)
+        long long o = n / 2;        // floor(n / 2)
+
+        long long res = (power(5, e) * power(4, o)) % MOD;
+        return res;
+    }
+};
