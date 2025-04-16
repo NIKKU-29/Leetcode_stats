b4cdd6ee4e152a6f78a3efb610e2916742135be9
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2025-04-16 18:24:48 +0530

    Time: 91 ms (34.6%), Space: 79 MB (77.96%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        
         long long count = 0;
         int n = nums.size();
-        int l = 0;
-        int r = 0;
-        unordered_map<long long,long long>mp;
-        int paircount = 0;
-
-        while(r < n)
-        {
-            paircount+=mp[nums[r]];
-            mp[nums[r]]++;
+        int l = 0, r = 0;
+        unordered_map<int, int> freq;
+        long long pairCount = 0;
+        
+        while (r < n) {
+            // Before adding the new element
+            int oldFreq = freq[nums[r]];
+            long long oldPairs = (long long)oldFreq * (oldFreq - 1) / 2;
             
-
-            while(paircount >= k )
-            {
+            // Add the new element
+            freq[nums[r]]++;
+            
+            // After adding the new element
+            int newFreq = freq[nums[r]];
+            long long newPairs = (long long)newFreq * (newFreq - 1) / 2;
+            
+            // Update the pair count with the difference
+            pairCount += (newPairs - oldPairs);
+            
+            // Shrink window from left if we have enough pairs
+            while (pairCount >= k) {
+                // Count all valid subarrays ending at or after r
                 count += n - r;
-                mp[nums[l]]--;
-                if(mp[nums[l]] == 0) mp.erase(nums[l]);
-                paircount-=mp[nums[l]];
+                
+                // Before removing the left element
+                oldFreq = freq[nums[l]];
+                oldPairs = (long long)oldFreq * (oldFreq - 1) / 2;
+                
+                // Remove the left element
+                freq[nums[l]]--;
+                
+                // After removing the left element
+                newFreq = freq[nums[l]];
+                newPairs = (long long)newFreq * (newFreq - 1) / 2;
+                
+                // Update the pair count with the difference
+                pairCount -= (oldPairs - newPairs);
+                
                 l++;
-
             }
-
+            
             r++;
         }
-
-
+        
         return count;
-
     }
 };
