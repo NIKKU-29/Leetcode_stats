bdf1c1a2eee776ea0b44b5a695ae9aa907130379
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-19 15:00:38 +0530

    Time: 26 ms (91.89%), Space: 60.2 MB (97.26%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,20 +1,57 @@
 class Solution {
 public:
-    long long solver(int curr, int low, int up, vector<int>& nums, int startIdx) {
-        return upper_bound(nums.begin() + startIdx, nums.end(), up - curr ) -
-               lower_bound(nums.begin() + startIdx, nums.end(), low - curr);
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        
+        sort(nums.begin(),nums.end());
+        return lower_B(nums,upper + 1) - lower_B(nums,lower);
     }
 
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long long count = 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+    private:
 
-        for (int i = 0; i < n; ++i) {
-            int curr = nums[i];
-            count += solver(curr, lower, upper, nums, i + 1);  // always safe
+        
+        long long lower_B(vector<int>&nums,int limit)
+        {
+            long long count = 0;
+            int left = 0;
+            int right = nums.size() - 1;
+
+            while(left < right)
+            {
+                int sum = nums[left] + nums[right];
+
+                if(sum < limit)
+                {
+                    count +=  right - left;
+                    left++;
+                }
+
+                else {
+                    right--;
+                }
+            }
+
+            return count;
         }
 
-        return count;
-    }
 };
+
+// class Solution {
+// public:
+//     long long solver(int curr, int low, int up, vector<int>& nums, int startIdx) {
+//         return upper_bound(nums.begin() + startIdx, nums.end(), up - curr ) -
+//                lower_bound(nums.begin() + startIdx, nums.end(), low - curr);
+//     }
+
+//     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+//         long long count = 0;
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; ++i) {
+//             int curr = nums[i];
+//             count += solver(curr, lower, upper, nums, i + 1);  // always safe
+//         }
+
+//         return count;
+//     }
+// };
