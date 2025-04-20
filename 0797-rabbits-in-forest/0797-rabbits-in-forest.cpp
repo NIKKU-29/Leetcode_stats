e31132ca83a1eef6fc8a9a2f7064767ceb573ad5
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-21 01:20:01 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.74%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -1,82 +1,83 @@
-// class Solution {
-// public:
-//     int numRabbits(vector<int>& answers) {
-        
-//         /*
-//             1->2                  2->3                  3->4
-//             1,1->2                2,2->3                3,3->4 
-//             1,1,1->4              2,2,2->3              3,3,3->4
-//             1,1,1,1->4            2,2,2,2->6            3,3,3,3->4
-//             1,1,1,1,1->6          2,2,2,2,2->6          3,3,3,3,3->8
-               
-//             1 wouch for 1         2 wouch for 3         3 wouch for 4
-//             1,1 wouch for 2       2,2 wouch for 3..      ............
-//             1,1,1 woucch for 4    ................ 
-
-
-//             similary u can observe that  num wouch for num + 1 rabits
-//             whem u have found how many num + 1 rabist u have
-//             just calculate how many of them are wouching for each other
-//             which is simple as doing % of count with num+1
-//             then the left rabbits woudl wouch for each other at min
-//             giving num +1
-
-//                     int d = count / (num + 1);
-//                     int q = count % (num + 1);
-//                     sum+= d*(num + 1);
-
-//                     if(q) sum+=(num+1);
-
-
-//                     here we can also use ceil but we have to convert the divin in double 
-
-
-//         */
-
-
-//             map<int,int>mp;
-//             for(auto elem : answers)
-//             {
-//                 mp[elem]++;
-//             }
-
-//             int sum = 0;
-
-//             for(auto elem : mp)
-//             {
-//                 int num = elem.first;
-//                 int count = elem.second;
-
-//                 if(count <= num + 1 )
-//                 {
-//                     sum+=(num+1);
-//                 }
-
-//                 else{
-                    
-//                     int d = count / (num + 1);
-//                     int q = count % (num + 1);
-//                     sum+= d*(num + 1);
-
-//                     if(q) sum+=(num+1);
-
-//                 }
-//             }
-
-//         return sum;
-//     }
-// };
-
-
-
 class Solution {
 public:
-    int numRabbits(vector<int>& nums, int total = 0) {
-        unordered_map <int,int> mpp;
-        for (int i : nums) mpp[i]++;
+    int numRabbits(vector<int>& answers) {
+        
+        /*
+            1->2                  2->3                  3->4
+            1,1->2                2,2->3                3,3->4 
+            1,1,1->4              2,2,2->3              3,3,3->4
+            1,1,1,1->4            2,2,2,2->6            3,3,3,3->4
+            1,1,1,1,1->6          2,2,2,2,2->6          3,3,3,3,3->8
+               
+            1 wouch for 1         2 wouch for 3         3 wouch for 4
+            1,1 wouch for 2       2,2 wouch for 3..      ............
+            1,1,1 woucch for 4    ................ 
 
-        for (auto& p : mpp)
-        total += ceil((float)p.second / (p.first + 1)) * (p.first + 1);
-        return total;
+
+            similary u can observe that  num wouch for num + 1 rabits
+            whem u have found how many num + 1 rabist u have
+            just calculate how many of them are wouching for each other
+            which is simple as doing % of count with num+1
+            then the left rabbits woudl wouch for each other at min
+            giving num +1
+
+                    int d = count / (num + 1);
+                    int q = count % (num + 1);
+                    sum+= d*(num + 1);
+
+                    if(q) sum+=(num+1);
+
+
+                    here we can also use ceil but we have to convert the divin in double 
+
+
+        */
+
+
+            map<int,int>mp;
+            for(auto elem : answers)
+            {
+                mp[elem]++;
+            }
+
+            int sum = 0;
+
+            for(auto elem : mp)
+            {
+                int num = elem.first;
+                int count = elem.second;
+
+                if(count <= num + 1 )
+                {
+                    sum+=(num+1);
+                }
+
+                else{
+                    
+                    // int d = count / (num + 1);
+                    // int q = count % (num + 1);
+                    // sum+= d*(num + 1);
+
+                    // if(q) sum+=(num+1);
+                    
+                    sum += ((count / (num + 1)) + (count % (num + 1) == 0 ? 0 : 1 )) * (num + 1);
+                }
+            }
+
+        return sum;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int numRabbits(vector<int>& nums, int total = 0) {
+//         unordered_map <int,int> mpp;
+//         for (int i : nums) mpp[i]++;
+
+//         for (auto& p : mpp)
+//         total += ceil((float or double)p.second / (p.first + 1)) * (p.first + 1);
+//         return total;
+//     }
+// };
