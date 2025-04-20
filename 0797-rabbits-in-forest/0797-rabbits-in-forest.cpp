8348c31e9de0737569dd893c8c9320df2bd6ead5
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-21 01:22:20 +0530

    Time: 2 ms (28.8%), Space: 12.2 MB (42.28%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -37,7 +37,7 @@ public:
             map<int,int>mp;
             for(auto elem : answers)
             {
-                mp[elem]++;
+                mp[elem + 1]++;
             }
 
             int sum = 0;
@@ -59,7 +59,7 @@ public:
                     // sum+= d*(num + 1);
                     // if(q) sum+=(num+1);
                     
-                    sum += ((count / (num + 1)) + (count % (num + 1) == 0 ? 0 : 1 )) * (num + 1);
+                    sum += ((count / num) + (count % num == 0 ? 0 : 1 )) * (num);
                 // }
             }
 
