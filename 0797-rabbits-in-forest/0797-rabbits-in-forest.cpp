a08e5e8f8a4757599aee67bd940a36070082eb9a
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-21 00:47:15 +0530

    Time: 1 ms (36.71%), Space: 12.2 MB (42.46%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -17,19 +17,15 @@ public:
 
                 if(num == 0) sum+=count;
 
-                else if(count <= num + 1 )
+                else if(! (count % num + 1) )
                 {
                     sum+=(num+1);
                 }
 
                 else{
                     
-                  
                     double d = ceil((double)count / (num + 1));
-                    
                     sum+= d * (num+1);
-                   
-                
                 }
             }
 
