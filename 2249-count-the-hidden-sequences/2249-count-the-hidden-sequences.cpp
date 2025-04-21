ea243cdb301d9ff1eedaceb0f6e1f80462c3f0a0
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-21 14:11:23 +0530

    Time: 1 ms (47.58%), Space: 112.6 MB (75%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long long sum = 0, min_sum = 0, max_sum = 0;
-        
-        for (int diff : differences) {
-            sum += diff;
-            min_sum = min(min_sum, sum);
-            max_sum = max(max_sum, sum);
+        long long maxi = 0;
+        long long mini = 0;
+        long long sum = 0;
+
+        for(auto elem : differences) {
+            sum += elem;
+            maxi = max(maxi, sum);
+            mini = min(mini, sum);
         }
 
-        long long left = lower - min_sum;
-        long long right = upper - max_sum;
+        if (maxi - mini > upper - lower)
+            return 0;
 
-        return max(0LL, right - left + 1);
+        return (upper - lower) - (maxi - mini) + 1;
     }
 };
