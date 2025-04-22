eacc32ca2055a948013dacc6c3063a4bbcd919ae
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-22 18:46:29 +0530

    Time: 0 ms (100%), Space: 10 MB (65.66%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-         int n = nums.size();
+       int n = nums.size();
     if (n < 2) return n;
     
-    // dp[i][0] = length of longest wiggle subsequence ending at index i with a negative last difference
-    // dp[i][1] = length of longest wiggle subsequence ending at index i with a positive last difference
-    vector<vector<int>> dp(n, vector<int>(2, 1));
-    
-    int result = 1;
+    // Initialize states: up represents length ending with an up difference,
+    // down represents length ending with a down difference
+    int up = 1, down = 1;
     
     for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            // If current number is greater than previous - can form positive difference
-            if (nums[i] > nums[j]) {
-                // We can extend a subsequence that ended with a negative difference
-                dp[i][1] = max(dp[i][1], dp[j][0] + 1);
-            }
-            // If current number is less than previous - can form negative difference
-            else if (nums[i] < nums[j]) {
-                // We can extend a subsequence that ended with a positive difference
-                dp[i][0] = max(dp[i][0], dp[j][1] + 1);
-            }
+        if (nums[i] > nums[i-1]) {
+            // Current element is greater - can form an up wiggle
+            // We extend the previous down sequence
+            up = down + 1;
+        } else if (nums[i] < nums[i-1]) {
+            // Current element is smaller - can form a down wiggle
+            // We extend the previous up sequence
+            down = up + 1;
         }
-        
-        // Update result with the maximum of both possibilities at current index
-        result = max({result, dp[i][0], dp[i][1]});
+        // If nums[i] == nums[i-1], we can't form a wiggle - up and down remain the same
     }
     
-    return result;
+    return max(up, down);
     }
 };
