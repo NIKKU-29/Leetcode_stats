e273002a094cccb173e85c60ac219f5c55c9221f
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-23 16:12:33 +0530

    Time: 422 ms (6.1%), Space: 8.2 MB (27.46%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+
+        unordered_map<int,int>mp;
+        int maxi = INT_MIN;
+
+        for(int i=1 ; i <= n ; i++)
+        {
+            int sum = 0;
+            int temp = i;
+            
+            while(temp)
+            {
+                sum+= (temp % 10);
+                temp/=10;
+                cout<<sum;
+            }
+
+            mp[sum]++;
+            maxi = max(maxi,mp[sum]);
+        }
+
+        int count = 0;
+
+        for(auto elem : mp)
+        {
+            if(elem.second == maxi) count++;
+        }
+
+        return count;
+        
+        return 0;
+    }
+};
