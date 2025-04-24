03cca660e30f17114f61058496732477ad085d78
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-04-24 10:43:03 +0530

    Time: 14 ms (83.8%), Space: 41.8 MB (71.22%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        
+        int r =0 , l = 0;
+        int n = nums.size();
+        unordered_map<int,int>mp;
+
+        for(auto elem : nums)
+        {
+            mp[elem]++;
+        }
+
+        unordered_map<int,int>Wmp;
+        int count = 0;
+
+        while(r < n)
+        {
+            Wmp[nums[r]]++;
+            // cout<<"HI";
+            // cout<<Wmp.size()<<"___"<<mp.size()<<"->"<<r<<endl;
+
+            while(Wmp.size() == mp.size() && l < n)
+            {
+                count+=(n-r);
+                Wmp[nums[l]]--;
+                if(Wmp[nums[l]] == 0) Wmp.erase(nums[l]);
+                l++;
+            }
+
+            r++;
+        }
+        
+        return count;
+
+    }
+};
