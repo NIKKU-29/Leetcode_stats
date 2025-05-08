e8e3a2d8bd700ef87cc0d4c82eb867e5cbdf3b7b
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-05-08 13:32:21 +0530

    Time: 1144 ms (22.57%), Space: 214.5 MB (17.51%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -2,42 +2,31 @@ class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& M) {
         int n = M.size(), m = M[0].size();
-        vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(m, vector<bool>(3, false)));
-        vector<pair<int,int>> directions = {{1,0},{-1,0},{0,-1},{0,1}};
+        using T = tuple<int, int, int, int>; // time, x, y, isFast (1 or 0)
         
-        using T = tuple<int, int, int, int>; // {time, variable, x, y}
-        priority_queue<T, vector<T>, greater<T>> q;
-
-        q.push({0, -1, 0, 0});  // Start with time=0, variable=-1, x=0, y=0
+        priority_queue<T, vector<T>, greater<T>> pq;
+        pq.push({0, 0, 0, 1}); // Start at (0,0), time = 0, first move is fast (1s)
         
-        while (!q.empty()) {
-            auto [currtime, variable, x, y] = q.top(); q.pop();
+        vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(m, vector<bool>(2, false)));
+        vector<pair<int, int>> directions = {{1,0},{-1,0},{0,-1},{0,1}};
+        
+        while (!pq.empty()) {
+            auto [currTime, x, y, isFast] = pq.top(); pq.pop();
             
-            int vIdx = (variable == -1) ? 0 : variable; // Map -1 → 0 index for visited
-
-            if (visited[x][y][vIdx]) continue;
-            visited[x][y][vIdx] = true;
+            if (x == n - 1 && y == m - 1)
+                return currTime;
             
-            if (x == n - 1 && y == m - 1) return currtime;
+            if (visited[x][y][isFast])
+                continue;
+            
+            visited[x][y][isFast] = true;
             
             for (auto [dx, dy] : directions) {
-                int newx = x + dx;
-                int newy = y + dy;
-                
-                if (newx >= 0 && newx < n && newy >= 0 && newy < m) {
-                    int nextTime;
-                    int newVariable;
-                    
-                    if (variable == -1 || variable == 2) {
-                        nextTime = max(currtime, M[newx][newy]) + 1;
-                        newVariable = 1;
-                    } else {
-                        nextTime = max(currtime, M[newx][newy]) + 2;
-                        newVariable = 2;
-                    }
-
-                    if (!visited[newx][newy][newVariable])
-                        q.push({nextTime, newVariable, newx, newy});
+                int newX = x + dx, newY = y + dy;
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
+                    int moveDuration = isFast ? 1 : 2;
+                    int nextTime = max(currTime , M[newX][newY]) + moveDuration;
+                    pq.push({nextTime, newX, newY, 1 - isFast});
                 }
             }
         }
