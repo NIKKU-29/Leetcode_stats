0476a5579e402e3e5aecabaf18e04bd0c160622e
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-06-08 22:10:30 +0530

    Time: 75 ms (13.65%), Space: 28 MB (11.28%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,84 +1,21 @@
-// class Solution {
-// public:
-
-//     static bool comp(const int & a, const int & b) {
-//         return to_string(a) < to_string(b);
-//     }
-    
-//     vector<int> lexicalOrder(int n) {
-
-//         vector<int>vec(n);
-//         iota(vec.begin(),vec.end(),1);
-//         sort(vec.begin(),vec.end(),comp);
-//         return vec;
-        
-//     }
-// };
-
-// class Solution {
-// public:
-//     vector<int> lexicalOrder(int n) {
-
-//         vector<int>Order;
-//         int currnum=1;
-
-//         for(int i=0;i<n;i++)
-//         {
-//             Order.push_back(currnum);
-
-//             if(currnum*10 <= n)
-//             {
-//                 currnum*=10;
-//             }
-
-//             else
-//             {
-//                 while(currnum%10 == 9 || currnum>=n)
-//                 {
-//                     currnum/=10;
-//                 }
-                
-//                 currnum+=1;
-//             }
-//         }
-
-//         return Order;
-//     }
-// };
-
 class Solution {
 public:
-
-    void solve(int curr,int n,vector<int>& ans)
-    {
-        if(curr>n) return;
-
-        ans.push_back(curr);
-
-
-        for(int i=0;i<=9;i++)
-        {
-            if(curr*10 + i > n)
-            {
-                return;
-            }
-
-            solve(curr*10 + i , n , ans);
-        }
-    }
-
-
     vector<int> lexicalOrder(int n) {
-
+        
         vector<int>ans;
-
-        for(int start=1;start<=9;start++)
+        vector<string>v;
+        for(int i = 1; i <= n ; i++)
         {
-            solve(start,n,ans);
+            v.push_back(to_string(i));
         }
 
+        sort(v.begin(),v.end());
+
+        for(auto elem : v)
+        {
+            ans.push_back(stoi(elem));
+        }
 
         return ans;
-        
     }
 };
