95482035d1795cef01e710adffad84cf64a93dc8
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-06-08 22:28:02 +0530

    Time: 0 ms (100%), Space: 14.2 MB (52.72%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
+
+    void solver(int curr , int limit, vector<int>&ans)
+    {
+        if(curr > limit) return;
+        ans.push_back(curr);
+
+        for(int i = 0 ; i <= 9 ; i++)
+        {
+            int next = curr * 10 + i;
+            if(next > limit) continue;
+            solver(next,limit,ans);
+        }
+    }
+
     vector<int> lexicalOrder(int n) {
         
+        // vector<int>ans;
+        // vector<string>v;
+        // for(int i = 1; i <= n ; i++)
+        // {
+        //     v.push_back(to_string(i));
+        // }
+
+        // sort(v.begin(),v.end());
+
+        // for(auto elem : v)
+        // {
+        //     ans.push_back(stoi(elem));
+        // }
+
+        // return ans;
+
         vector<int>ans;
-        vector<string>v;
-        for(int i = 1; i <= n ; i++)
-        {
-            v.push_back(to_string(i));
-        }
 
-        sort(v.begin(),v.end());
-
-        for(auto elem : v)
+        for(int i = 1 ; i <= 9; i++)
         {
-            ans.push_back(stoi(elem));
+            solver(i,n,ans);
         }
 
         return ans;
