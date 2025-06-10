d3dd74dd7039541e343676ce770b9d12efd28865
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-06-11 01:35:43 +0530

    Time: 3 ms (34.13%), Space: 10 MB (16.81%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -2,36 +2,58 @@ class Solution {
 public:
     int maxDifference(string s) {
 
-        vector<int>mp(26,0);
+        // vector<int>mp(26,0);
+        // for(auto elem : s)
+        // {
+        //     mp[elem - 'a']++;
+        // }
+
+        // int mini = INT_MAX;
+        // int maxi = INT_MIN;
+
+        // for(auto elem : mp)
+        // {
+        //     if(elem % 2)
+        //     {
+        //         maxi = max(maxi,elem);
+        //         // cout<<maxi<<">>>";
+        //         // cout<<endl;
+        //     }
+
+        //     else if(elem > 0 )
+        //     {
+        //         mini = min(mini,elem); 
+        //         // cout<<mini<<"<<<<";
+        //         // cout<<endl;   
+        //     }
+        // }
+
+        // return maxi - mini;
+
+        unordered_map<char,int>mp;
 
         for(auto elem : s)
         {
-            mp[elem - 'a']++;
+            mp[elem]++;
         }
 
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        int f1 = INT_MIN;
+        int f2 = INT_MAX;
 
-        for(auto elem : mp)
+        for(auto& [key,value] : mp)
         {
-            if(elem % 2)
+            if(value % 2)
             {
-                maxi = max(maxi,elem);
-                // cout<<maxi<<">>>";
-                // cout<<endl;
+                f1 = max(f1,value);
             }
 
-            else if(elem > 0 )
-            {
-                mini = min(mini,elem); 
-                // cout<<mini<<"<<<<";
-                // cout<<endl;   
+            else{
+                   f2 = min(f2,value);
             }
         }
 
-        
 
-        return maxi - mini;
+        return f1 - f2;
 
     }
 };
