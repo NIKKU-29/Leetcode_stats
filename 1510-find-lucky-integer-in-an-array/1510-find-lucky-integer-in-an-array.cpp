d992f27ce345a0e53654e9f4803cf33838822a61
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-07-05 22:53:01 +0530

    Time: 0 ms (100%), Space: 13.9 MB (65.75%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>freq(501);
         int ans = -1;
         for(auto elem : arr) freq[elem]++;
-        for(int i = 1 ; i< 500 ; i++) 
+        for(int i = 1 ; i<= 500 ; i++) 
             if(i == freq[i]) ans = max(ans,i);
         return ans;
     }   
