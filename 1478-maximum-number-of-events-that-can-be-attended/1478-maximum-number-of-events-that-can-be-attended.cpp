0afb83680e5a84625c9d6c8bcfe787effe87d879
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-07-08 03:06:51 +0530

    Time: 282 ms (8.98%), Space: 134.5 MB (7.71%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        set<int> s;
 
-        // Sort events by end date first, then by start date
-        sort(events.begin(), events.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
-             });
+        set<int>s;
 
-        // Find the maximum end date to limit the size of the set
-        int maxEnd = 0;
-        for (const auto& event : events) {
-            maxEnd = max(maxEnd, event[1]);
-        }
+        sort(events.begin(),events.end(),[](const vector<int>&a,const vector<int>&b){
 
-        // Initialize the set with dates from 1 to maxEnd
-        for (int i = 1; i <= maxEnd; i++) {
-            s.insert(i);
-        }
+                return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
+        });
 
-        int cnt = 0;
-        for (const auto& event : events) {
-            int start = event[0];
-            int end = event[1];
+        int maxe = 0;
+        for(auto elem : events) maxe = max(maxe,elem[1]);
+        for(int i = 1; i <= maxe ; i++) s.insert(i);
 
+        int count = 0;
+        for(auto ele : events)
+        {
+            int start = ele[0];
+            int end = ele[1];
+            
             auto it = s.lower_bound(start);
-            if (it == s.end() || *it > end) {
-                continue;
-            } else {
-                cnt++;
-                s.erase(it);
-            }
+            if(it == s.end() || *it > end) continue;
+            else count++; s.erase(it);
         }
-
-        return cnt; // Return the number of days attended
+        
+        return count;
     }
 };
