98adbf71410dd8feae68cbfd3a86a3b04eab2be3
Nemish Sharma <113784295+NIKKU-29@users.noreply.github.com>
2025-07-15 15:12:22 +0530

    Time: 0 ms (100%), Space: 8.1 MB (33.01%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     bool isValid(string word) {
-        bool char3 = false;
-        bool vow = false;
-        bool cons = false;
-        int chr = 0;
+        bool hasVowel = false, hasConsonant = false;
+        int count = 0;
 
-        for(auto elem : word)
-        {
-            if(!isalpha(elem) && !isdigit(elem)) return false;
-            if(isalpha(elem))
-            {
-                elem = toupper(elem);
-                if(elem == 'A' || elem == 'E' || elem == 'I' || elem == 'O' || elem == 'U')
-                {
-                    vow = true;
-                    chr++;
-                }
+        for (char c : word) {
+            if (!isalnum(c)) return false;  
 
-                else{
+            count++;
 
-                    cons = true;
-                    chr++;
-
-                }
+            if (isalpha(c)) {
+                char upper = toupper(c);
+                if (upper == 'A' || upper == 'E' || upper == 'I' || upper == 'O' || upper == 'U')
+                    hasVowel = true;
+                else
+                    hasConsonant = true;
             }
-
-            if(isdigit(elem)) chr++;
         }
 
-        if(chr >= 3) char3 = true;
-
-       
-        return char3 && cons && vow ;
+        return count >= 3 && hasVowel && hasConsonant;
     }
 };
